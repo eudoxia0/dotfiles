27aa411f0b5530e96dc040e8cafe70e76a4ed4d4
Fernando Borretti <eudoxiahp@gmail.com>
2013-10-11 15:09:46 -0200

    Change a shitload of things

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -1,4 +1,54 @@
-;;; Lisp
+;;;     "Show me your ~/.emacs and I will tell you who you are."
+;;;                                        Bogdan Maryniuk
+
+;; Fire up server
+(require 'server)
+;;Start emacs server in running GUI and not already running
+(setq server-socket-dir "/tmp/emacs-shared")
+(if (display-graphic-p)
+    (unless (server-running-p)
+      (server-start)))
+
+;;; Themes
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+(load-theme 'dichromacy t)
+
+;;; Basic interface options
+
+(mouse-wheel-mode t)
+(setq require-final-newline t)
+
+;; Hide scrollbars
+(scroll-bar-mode -1)
+;; Hide toolbar
+(tool-bar-mode -1)
+(menu-bar-mode 0)
+;; Empty scratch buffer
+(setq initial-scratch-message "")
+;; Don't show startup screen
+(setq inhibit-splash-screen t)
+;; Always y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(setq-default cursor-type 'bar)
+;; Show column number
+(setq column-number-mode  t)
+
+;; Always use spaces when indenting (unless overridden for buffer)
+(setq-default indent-tabs-mode nil)
+
+(setq vc-follow-symlinks t)
+
+;; Highlight line
+(global-hl-line-mode)
+
+;; Font options
+(set-face-attribute 'default nil :font "Monaco")
+(set-face-attribute 'default nil :height 100)
+
+;;; Common Lisp options
 
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "sbcl")
@@ -15,41 +65,21 @@
 
 (setq slime-enable-evaluate-in-emacs t)
 
-;;; Basic interface options
-
-(mouse-wheel-mode t)
-(setq require-final-newline t)
-
-;; Hide scrollbars
-(scroll-bar-mode -1)
-;; Hide toolbar
-(tool-bar-mode -1)
-(menu-bar-mode 0)
-;; Empty scratch buffer
-(setq initial-scratch-message "")
-;; Don't show startup screen
-(setq inhibit-splash-screen t)
-;; Always y/n o p
-(fset 'yes-or-no-p 'y-or-n-p)
-
-(set-face-attribute 'default nil :font "Monaco")
-(set-face-attribute 'default nil :height 100)
-
 ;;; Packages
 
 (require 'package)
-(package-initialize)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
 
 (defvar packages
   '(org nav
     ;; Major modes
     yaml-mode clojure-mode d-mode textile-mode markdown-mode gnuplot-mode
-    erlang haskell-mode    
+    erlang haskell-mode fsharp-mode sass-mode rainbow-mode ruby-mode
     ;; Other plugins
-    rainbow-delimiters))
+    rainbow-delimiters magit ecb))
 
 (defun packages-installed-p ()
   (not (memq 'nil (mapcar (lambda (p) (package-installed-p p)) packages))))
@@ -71,27 +101,29 @@
 
 ;;; Modes
 
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.d$" . d-mode))
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.textile$" . textile-mode))
-(add-to-list 'auto-mode-alist '("\\.gp$" . gnuplot-mode))
-(add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
+(setq auto-mode-alist
+      (append '(("\\.md\\'" . markdown-mode)
+		("\\.\\(yaml\\|yml\\)$" . yaml-mode)
+		("\\.d$" . d-mode)
+		("\\.clj$" . clojure-mode)
+		("\\.textile$" . textile-mode)
+		("\\.gp$" . gnuplot-mode)
+		("\\.sql$" . sql-mode)
+		("\\.\\(rb\\|Gemfile\\|Rakefile\\)$" . ruby-mode)
+		("\\.hs$" . haskell-mode))
+	      auto-mode-alist))
 
-;;; Load themes
+;;; Keybindings
 
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-
-(load-theme 'dichromacy t)
+(global-set-key (kbd "<f8>")
+                (lambda()(interactive)(find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "C-.") 'magit-status)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("29a4267a4ae1e8b06934fec2ee49472daebd45e1ee6a10d8ff747853f9a3e622" "dc46381844ec8fcf9607a319aa6b442244d8c7a734a2625dac6a1f63e34bc4a6" "f220c05492910a305f5d26414ad82bf25a321c35aa05b1565be12f253579dec6" "c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" default))))
+ '(ecb-options-version "2.40"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
