63c1ba6ef776fd122639e8a12271f6b2fbf15ecc
Fernando Borretti <eudoxiahp@gmail.com>
2013-10-31 20:03:06 -0200

    Whoops forgot about this file

---
+++ home/.shell/var.sh
@@ -0,0 +1,11 @@
+# Let's make Java a little less ugly (lol)
+
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on'
+
+# virtualenvwrapper
+export WORKON_HOME=~/.virtualenvs
+export PIP_VIRTUALENV_BASE=$WORKON_HOME
+export PIP_RESPECT_VIRTUALENV=true
+
+# NPM
+export PATH="$PATH:$HOME/.npm/bin"
