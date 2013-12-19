c491d820fe6fcc1e098252c3b52cedad38e2d5f0
Fernando Borretti <eudoxiahp@gmail.com>
2013-12-19 16:57:45 -0200

    Add SLIME images support

--- home/.emacs.d/lisp.el
+++ home/.emacs.d/lisp.el
@@ -9,12 +9,24 @@
 (set-language-environment "utf-8")
 (setq slime-net-coding-system 'utf-8-unix)
 
-
 (slime-setup '(slime-repl
                slime-asdf
                slime-fuzzy
                slime-banner
                slime-indentation
+               slime-xref-browser
                slime-media))
 
 (setq slime-enable-evaluate-in-emacs t)
+
+(defun nm-slime-media-insert-image (image string)
+  (with-current-buffer (slime-output-buffer)
+    (let ((marker (slime-output-target-marker :repl-result)))
+      (goto-char marker)
+      (slime-propertize-region `(face slime-repl-result-face
+                                      rear-nonsticky (face))
+        (insert-image image string)
+        (insert ?\n))
+      ;; Move the input-start marker after the REPL result.
+      (set-marker marker (point)))
+    (slime-repl-show-maximum-output)))
