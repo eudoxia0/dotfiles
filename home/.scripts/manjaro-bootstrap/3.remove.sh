bfbb65c39ed0221b3769e492fedba5477a9acd41
Fernando Borretti <eudoxiahp@gmail.com>
2014-03-03 23:23:35 -0200

    Don't use xnoise

--- home/.scripts/manjaro-bootstrap/3.remove.sh
+++ home/.scripts/manjaro-bootstrap/3.remove.sh
@@ -4,4 +4,4 @@
 
 sudo pacman -Rns conky nitrogen parcellite synapse geany lxterminal tint2 \
   obconf tintwizard qupzilla openbox obmenu-generator openbox-themes \
-  lxappearance-obconf thunar thunar-archive-plugin thunar-volman
+  lxappearance-obconf thunar thunar-archive-plugin thunar-volman xnoise
