22177b3af3d35d045a4f03a9625047d1a066b59c
Fernando Borretti <eudoxiahp@gmail.com>
2014-03-18 19:19:57 -0300

    Add a script to download my irssi scripts

---
+++ home/.scripts/bootstrap/irssi.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+irssi-scriptman.sh nickcolor http://scripts.irssi.org/scripts/nickcolor.pl
+irssi-scriptman.sh adv_windowlist anti.teamidiot.de/static/nei/*/Code/Irssi/adv_windowlist.pl
