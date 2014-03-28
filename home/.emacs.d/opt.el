fd3c4a8f6d7849d9a4fa40891f1c699e485a9c42
Fernando Borretti <eudoxiahp@gmail.com>
2014-03-28 15:50:43 -0300

    Change python checker to epylint2

--- home/.emacs.d/opt.el
+++ home/.emacs.d/opt.el
@@ -76,7 +76,7 @@
 (add-hook 'python-mode-hook 'flymake-mode)
 (add-hook 'common-lisp-mode 'flymake-mode)
 
-(setq pycodechecker "epylint")
+(setq pycodechecker "epylint2")
 (when (load "flymake" t)
   (defun flymake-pycodecheck-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
