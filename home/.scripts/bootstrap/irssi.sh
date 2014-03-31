a243bffae049611644ea3878c2ad99313cc75e0d
Fernando Borretti <eudoxiahp@gmail.com>
2014-03-31 19:46:44 -0300

    Add multipaste script

--- home/.scripts/bootstrap/irssi.sh
+++ home/.scripts/bootstrap/irssi.sh
@@ -2,3 +2,4 @@
 
 irssi-scriptman.sh nickcolor http://scripts.irssi.org/scripts/nickcolor.pl
 irssi-scriptman.sh adv_windowlist anti.teamidiot.de/static/nei/*/Code/Irssi/adv_windowlist.pl
+irssi-scriptman.sh multipaste http://scripts.irssi.org/scripts/multipaste.pl
