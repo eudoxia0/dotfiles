51a99e49216776f1f6d21f05060c1545d68a6a69
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-02 19:22:58 -0300

    Update install and delete lists in manjaro-bootstrap

--- home/.scripts/manjaro-bootstrap/0.update.sh
+++ home/.scripts/manjaro-bootstrap/0.update.sh
@@ -12,7 +12,7 @@ sudo pacman-optimize
 
 sudo pacman -Rns conky nitrogen parcellite synapse geany obconf tintwizard \
   qupzilla openbox obmenu-generator openbox-themes lxappearance-obconf thunar \
-  thunar-archive-plugin thunar-volman xnoise
+  thunar-archive-plugin thunar-volman xnoise evince
 
 # Update
 
