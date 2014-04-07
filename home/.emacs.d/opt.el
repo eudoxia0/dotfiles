b5c558e5f5e895fe84b64a2c39c57177a40f071f
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-07 19:52:35 -0300

    epylint is annoying as fuck

--- home/.emacs.d/opt.el
+++ home/.emacs.d/opt.el
@@ -76,16 +76,4 @@
 (add-hook 'python-mode-hook 'flymake-mode)
 (add-hook 'common-lisp-mode 'flymake-mode)
 
-(setq pycodechecker "epylint2")
-(when (load "flymake" t)
-  (defun flymake-pycodecheck-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list pycodechecker (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pycodecheck-init)))
-
 ;; CEDET
