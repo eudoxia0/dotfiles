358596e3781aebf40f846d9a83806700af0b9b33
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-08 15:35:39 -0300

    Remove stuff from .init.lisp, use CIM-installed quicklisp

--- home/.init.lisp
+++ home/.init.lisp
@@ -1,48 +1,14 @@
 ;;;; -*- Lisp -*-
 
-
 (setf *print-case* :downcase)
 
 ;;; The following lines added by ql:add-to-init-file:
+
 #-quicklisp
-(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-                                       (user-homedir-pathname))))
-  (when (probe-file quicklisp-init)
-    (load quicklisp-init)))
-
-(require :swank)
-
-;;; Better printing for hash tables
-(set-pprint-dispatch 'hash-table
- (lambda (str ht)
-  (format str "{~{~{~S => ~S~}~^, ~}}"
-   (loop for key being the hash-keys of ht
-         for value being the hash-values of ht
-         collect (list key value)))))
-
-;;; Better printing for objects
-
-#|
-(require :mop-utils)
-
-(defun class-slots (obj)
-  (mop-utils:slot-names-of obj))
-
-(defun slot-values (obj)
-  (loop for slot in (class-slots obj)
-        collecting
-        (list (symbol-name slot) (slot-value obj slot))))
-
-(set-pprint-dispatch 'standard-object
-  (lambda (str obj)
-    (format str "{~{~{~S => ~S~}~^, ~}}"
-            (slot-values obj))))|#
+(load (merge-pathnames ".cim/quicklisp/setup.lisp"
+                       (user-homedir-pathname)))
 
 ;;; Safety first!
 (declaim (optimize (safety 3) (debug 3) (speed 0)))
 
 (sb-ext:restrict-compiler-policy 'debug 3)
-
-(defun image (src)
-  (swank:eval-in-emacs
-   `(nm-slime-media-insert-image (create-image ,src) ,src)))
