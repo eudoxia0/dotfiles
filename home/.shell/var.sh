1506df3d0d1ee080d75b16fd8861c5b26d9b0e90
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-08 15:33:58 -0300

    Add ruby gems binary path and CIM binary path

--- home/.shell/var.sh
+++ home/.shell/var.sh
@@ -8,3 +8,8 @@ source /usr/bin/virtualenvwrapper.sh
 
 # NPM
 export PATH="$PATH:$HOME/.npm/bin:$HOME/.scripts"
+
+# Ruby
+export PATH="$PATH:$HOME/.gem/ruby/2.1.0/bin"
+
+CIM_HOME=/home/eudoxia/.cim; [ -s '/home/eudoxia/.cim/init.sh' ] && . '/home/eudoxia/.cim/init.sh'
