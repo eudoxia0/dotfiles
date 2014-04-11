82d485daf68b3ebfdf7235a704f836db5c7adc26
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-11 19:31:19 -0300

    Change multihead layout

--- home/.screenlayout/home.sh
+++ home/.screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS --mode 1280x768 --pos 1600x128 --rotate normal --output CRT1 --mode 1600x900 --pos 0x0 --rotate normal --output DFP1 --off
+xrandr --output LVDS --mode 1366x768 --pos 1600x132 --rotate normal --output CRT1 --mode 1600x900 --pos 0x0 --rotate normal --output DFP1 --off
