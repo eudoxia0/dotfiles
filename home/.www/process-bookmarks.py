59f8906ab32ed677267ae48e447783963cd16ce3
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-15 17:48:55 -0300

    Update process-bookmarks.py

--- home/.www/process-bookmarks.py
+++ home/.www/process-bookmarks.py
@@ -5,15 +5,23 @@ root = json.loads(open('bookmarks.1.json').read())
 
 delete_fields = ['dateAdded', 'guid', 'id', 'index', 'lastModified', 'root',
                  'type', 'parent', 'charset', 'annos']
+TITLE_KEY = 'label'
 
 def step(node):
-    has_children = 'children' in node
+    has_children = 'children' in node or 'sub' in node
     for field in delete_fields:
         if field in node:
             del node[field]
+    node[TITLE_KEY] = node['title']
+    del node['title']
     if has_children:
-        node['children'] = [step(child) for child in
-                            node['children']]
+        # Folder
+        node['sub'] = [step(child) for child in
+                       node['children']+node.get('sub',[])]
+        del node['children']
+    else:
+        # Bookmark
+        node = [node[TITLE_KEY], node['uri']]
     return node
 
 data = [step(child) for child in root['children']]
