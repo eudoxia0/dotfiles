05b400ed77a45198a89dc1e16e103f6517e815de
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-24 19:25:37 -0300

    Add nfs, set it up

--- home/.scripts/manjaro-bootstrap/2.install.sh
+++ home/.scripts/manjaro-bootstrap/2.install.sh
@@ -3,13 +3,14 @@
 # Install packages
 
 sudo pacman -S yaourt rxvt-unicode git firefox bitcoin-qt sqlite3 nss feh \
-  redshift rtorrent make automake emacs unison pcmanfm giflib gimp gimp \
+  redshift rtorrent make automake emacs unison pcmanfm giflib gimp \
   inkscape terminus-font autoconf libtool autogen clang llvm keepass maxima \
   gnuplot patch meld scrot calibre tint2 banshee cmake bzr vala python2-pip \
   screenfetch numlockx scala sbt ruby bison graphviz ditaa virtualbox nodejs \
   units pcmanfm ansible newsbeuter bash-completion skype python2-virtualenv \
   python-virtualenvwrapper python2-pylint r net-tools virtualbox-host-modules \
-  zathura zathura-pdf-mupdf zathura-djvu xmonad xmonad-contrib gmrun
+  zathura zathura-pdf-mupdf zathura-djvu xmonad xmonad-contrib gmrun xcompmgr \
+  nfs-utils
 
 # Yaourt packages
 sudo yaourt -S --noconfirm ttf-win7-fonts-autodownload tor-browser-en \
@@ -43,3 +44,8 @@ backup.sh
 
 # Now that we have init.lisp:
 sbcl --eval "(ql:quickload :quicklisp-slime-helper)" --quit
+
+# Setup NFS services (For Vagrant)
+sudo systemctl enable rpc-idmapd
+sudo systemctl enable rpc-mountd
+
