d6b365de8e0c7e3a239e1119ee9cde877785278e
Fernando Borretti <eudoxiahp@gmail.com>
2014-04-24 19:25:14 -0300

    Move PS1 to .shell/var.sh

--- home/.shell/var.sh
+++ home/.shell/var.sh
@@ -12,4 +12,8 @@ export PATH="$PATH:$HOME/.npm/bin:$HOME/.scripts"
 # Ruby
 export PATH="$PATH:$HOME/.gem/ruby/2.1.0/bin"
 
+#Lisp
 CIM_HOME=/home/eudoxia/.cim; [ -s '/home/eudoxia/.cim/init.sh' ] && . '/home/eudoxia/.cim/init.sh'
+
+# General
+PS1="\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\$\[\033[01m\][ \[\033[01;34m\]\u@\h \[\033[00m\]\[\033[01m\]] \[\033[01;32m\]\w\[\033[00m\]\n\[\033[01;34m\]$\[\033[00m\]> "
