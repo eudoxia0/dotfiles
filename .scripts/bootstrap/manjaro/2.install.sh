69a9b0f85c7c7fa4aafc3e6f7b91876a1343d1c4
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-04 19:20:40 -0300

    Update install script

--- .scripts/bootstrap/manjaro/2.install.sh
+++ .scripts/bootstrap/manjaro/2.install.sh
@@ -5,19 +5,19 @@
 sudo pacman -S yaourt rxvt-unicode git firefox bitcoin-qt sqlite3 nss feh \
   redshift transmission-gtk make automake emacs unison pcmanfm giflib gimp \
   inkscape terminus-font autoconf libtool autogen clang llvm keepass maxima \
-  gnuplot patch meld scrot calibre tint2 banshee cmake bzr vala python2-pip \
+  gnuplot patch meld scrot calibre tint2 cmake bzr vala python2-pip \
   screenfetch numlockx scala sbt ruby bison graphviz ditaa virtualbox nodejs \
   units pcmanfm ansible newsbeuter bash-completion skype python2-virtualenv \
   python-virtualenvwrapper python2-pylint r net-tools virtualbox-host-modules \
   zathura zathura-pdf-mupdf zathura-djvu xmonad xmonad-contrib gmrun xcompmgr \
   nfs-utils gdb valgrind hunspell hunspell-en python-sphinx erlang povray \
-  adobe-source-sans-pro-fonts doxygen nimrod blender tidyhtml sbcl asciidoc
+  adobe-source-sans-pro-fonts doxygen nimrod blender tidyhtml sbcl asciidoc \
+  pidgin pidgin-otr irssi
 
 # Yaourt packages
 sudo yaourt -S --noconfirm ttf-win7-fonts-autodownload tor-browser-en \
-  dogecoin-qt litecoin-qt electrum cpuminer pandoc-static rbenv ruby-build \
-  vagrant-git rbenv-default-gems heroku-toolbelt ttf-monaco grive tsung \
-  libfixposix wrk
+  electrum pandoc-static rbenv ruby-build vagrant-git rbenv-default-gems \
+  heroku-toolbelt ttf-monaco grive tsung libfixposix wrk
 
 # Setup NFS services (For Vagrant)
 sudo systemctl enable rpc-idmapd
