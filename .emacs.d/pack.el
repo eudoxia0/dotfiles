a3cd6094df49c802df9e0e8b669b9badadc9dd8e
Fernando Borretti <eudoxiahp@gmail.com>
2014-08-07 16:27:55 -0300

    Update screenlayout and pack.el

--- .emacs.d/pack.el
+++ .emacs.d/pack.el
@@ -15,9 +15,7 @@
     scala-mode2 ensime powerline rust-mode tuareg pov-mode adoc-mode
     ;; Other plugins
     rainbow-delimiters magit ecb auto-complete ag highlight-indentation
-    google-c-style flycheck ido flyspell rw-hunspell
-    ;; Other
-    gnugo
+    google-c-style flycheck ido flyspell rw-hunspell gnugo
     ;; Themes
     color-theme-solarized color-theme-twilight color-theme-github
     soft-morning-theme zenburn-theme qsimpleq-theme noctilux-theme
--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS --mode 1366x768 --pos 1600x132 --rotate normal --output CRT1 --mode 1600x900 --pos 0x0 --rotate normal --output DFP1 --off
+xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1600x132 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal
