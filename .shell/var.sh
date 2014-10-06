624f6a8986668a1a2453d0609e946dcd56672882
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-06 16:58:16 -0200

    Fix rbenv binary paths

--- .shell/var.sh
+++ .shell/var.sh
@@ -10,7 +10,8 @@ export LESS=' -R '
 export PATH="$PATH:$HOME/.npm/bin:$HOME/.scripts:$HOME/.cabal/bin"
 
 # Ruby
-export PATH="$PATH:$HOME/.gem/ruby/2.1.0/bin"
+export PATH="$PATH:$HOME/.rbenv/versions/2.1.0/bin"
+export PATH="$PATH:$HOME/.rbenv/versions/2.2.0-dev/bin"
 
 # General
 PS1="\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\$\[\033[01m\][ \[\033[01;34m\]\u@\h \[\033[00m\]\[\033[01m\]] \[\033[01;32m\]\w\[\033[00m\]\n\[\033[01;34m\]$\[\033[00m\]> "
