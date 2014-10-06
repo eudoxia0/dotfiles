05f4697108e1a3c34ea84e3ee7456175c3a50e0a
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-06 16:59:18 -0200

    Add tools to generate Markdown docs

--- tools.lisp
+++ tools.lisp
@@ -1,7 +1,26 @@
 (ql:quickload (list :drakma :cl-json :trivial-utf-8))
 
+;;; JSON requests
+
 (defun json-req (&rest params)
   (cl-json:decode-json-from-string
    (trivial-utf-8:utf-8-bytes-to-string
     (apply #'drakma:http-request params))))
 
+;;; Markdown documentation generation
+
+(defun document-function (fn)
+  (format nil "* `~A ~A`: ~A"
+          (swank-backend:function-name fn)
+          (swank-backend:arglist fn)
+          (documentation fn t)))
+
+(defun reintern (symbol package)
+  (intern (symbol-name symbol) (find-package package)))
+
+(defmacro docgen (package &rest body)
+  `(format t "~{~A~%~%~}"
+           (list ,@(loop for elem in body collecting
+                     (if (stringp elem)
+                         elem
+                         `(document-function #',(reintern elem package)))))))
