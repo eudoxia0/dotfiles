26ed86ca876bfb05f539acf8cb299dbe9762e9d0
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-16 21:31:09 -0200

    Move file type mode mappings to its own file

---
+++ .emacs.d/file-modes.el
@@ -0,0 +1,21 @@
+;;;; Map filetypes and special filenames to modes
+
+(setq auto-mode-alist
+      (append '(("\\.md\\'" . markdown-mode)
+		("\\.\\(yaml\\|yml\\)$" . yaml-mode)
+		("\\.d$" . d-mode)
+		("\\.clj$" . clojure-mode)
+		("\\.textile$" . textile-mode)
+		("\\.gp$" . gnuplot-mode)
+		("\\.sql$" . sql-mode)
+		("\\.\\(rb\\|Gemfile\\|Vagrantfile\\|Rakefile\\)$" . enh-ruby-mode)
+		("\\.hs$" . haskell-mode)
+                ("\\.dot$" . graphviz-dot-mode)
+                ("\\.doc$" . adoc-mode)
+                ;; Web modes
+                ("\\.html$" . web-mode)
+                ("\\.tmpl$" . web-mode)
+                ("\\.eco$" . web-mode)
+                ("\\.scss$" . sass-mode)
+                ("\\.lass\\'" . lass-mode))
+	    auto-mode-alist))
