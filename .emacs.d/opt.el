5c3eb2a949dd351073950de773a4623d29c59429
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-16 21:30:48 -0200

    Move ido mode stuff to its own file

--- .emacs.d/opt.el
+++ .emacs.d/opt.el
@@ -7,10 +7,5 @@
 ;; Powerline
 (require 'powerline)
 
-;; ido
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
 ;; Flycheck
 (add-hook 'nxml-mode-hook 'flycheck-enable-except-on-temp-buffers)
---
+++ .emacs.d/setup-ido.el
@@ -0,0 +1,5 @@
+;;;; Interactively do things
+
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
