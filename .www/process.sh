b4a115372f91b99e50b04371bc05c2caf51c8d4a
Fernando Borretti <eudoxiahp@gmail.com>
2014-10-20 17:33:14 -0200

    Delete this whole folder, since I've moved it to another personal project

--- .www/process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# The default JSON is horrifying. Clean it up.
-cat bookmarks.json | python -m json.tool > bookmarks.1.json
-# Call the Python parser
-python process-bookmarks.py > raw-bookmarks.yaml
-# Clean up
-rm bookmarks.1.json
