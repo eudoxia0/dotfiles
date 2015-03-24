1b38d94b7f3f920d97f370b8b14cebc29e7b7132
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-24 16:43:55 -0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function set_up () {
+  echo -e "\e[31mSetting up $1...\e[0m"
+  cd $2
+  bash install.sh
+  cd ..
+}
+
+set_up "Common Lisp" lisp
+set_up "Emacs" emacs
+set_up "shell" shell
+set_up "X11" x11
+set_up "StumpWM" stumpwm
+set_up "scripts" scripts
+set_up "other configuration" config
--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -avh --exclude "README.md" --exclude ".git/" --exclude "setup.sh" . ~
