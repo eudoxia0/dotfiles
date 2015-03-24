f61d5033843414949c42f108386939978078e459
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-24 16:42:55 -0300

    Merge some misc config files into a directory

---
+++ config/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DOSBOX_DIR=~/.dosbox
+RSS_DIR=~/.newsbeuter
+
+if [ ! -d "$DOSBOX_DIR" ]; then
+  mkdir $DOSBOX_DIR
+fi
+
+if [ ! -d "$RSS_DIR" ]; then
+  mkdir $RSS_DIR
+fi
+
+cp dosbox.conf ~/.dosbox/dosbox-0.74.conf
+cp newsbeuter.conf ~/.newsbeuter/config
+cp ocamlinit.ml ~/.ocamlinit
+cp psqlrc ~/.psqlrc
+cp gitconfig ~/.gitconfig
