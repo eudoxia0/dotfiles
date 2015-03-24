3f6b89d8c43661d1cccd206ef80b5f8c9ab095e4
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-24 16:38:30 -0300

    Delete some unused scripts

--- .scripts/bootstrap/irssi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-irssi-scriptman.sh nickcolor http://scripts.irssi.org/scripts/nickcolor.pl
-irssi-scriptman.sh adv_windowlist anti.teamidiot.de/static/nei/*/Code/Irssi/adv_windowlist.pl
-irssi-scriptman.sh multipaste http://scripts.irssi.org/scripts/multipaste.pl
--- .scripts/start-emacs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -e /tmp/emacs-shared/server ]; then
-    emacsclient -c -s /tmp/emacs-shared/server
-else
-    emacs
-fi
