ae07217601c66de5e1f1d383a87df48125252911
Fernando Borretti <eudoxiahp@gmail.com>
2015-03-26 18:09:26 -0300

    Change layout script

--- x11/layout.sh
+++ x11/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 120x900 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal
+xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 120x900 --rotate normal --output DP1 --off --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal
