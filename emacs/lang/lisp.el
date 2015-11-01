1d7bc3a02e8ee7e2bf519068d3ce9c36507493d8
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-01 20:47:13 -0300

    Use Sly for Lisp

--- emacs/lang/lisp.el
+++ emacs/lang/lisp.el
@@ -1,37 +1,9 @@
 ;;;; Configuration for Lisp modes
 
-;;; Common Lisp options
+;;; Common Lisp
 
-(load (expand-file-name "~/.quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
-
-(set-language-environment "utf-8")
-(setq slime-net-coding-system 'utf-8-unix)
-
-(slime-setup '(slime-repl
-               slime-asdf
-               slime-fuzzy
-               slime-banner
-               slime-indentation
-               slime-xref-browser
-               slime-fancy
-               slime-media
-               ;slime-company
-               ))
-
-(setq slime-enable-evaluate-in-emacs t)
-
-(defun nm-slime-media-insert-image (image string)
-  (with-current-buffer (slime-output-buffer)
-    (let ((marker (slime-output-target-marker :repl-result)))
-      (goto-char marker)
-      (slime-propertize-region `(face slime-repl-result-face
-                                      rear-nonsticky (face))
-        (insert-image image string)
-        (insert ?\n))
-      ;; Move the input-start marker after the REPL result.
-      (set-marker marker (point)))
-    (slime-repl-show-maximum-output)))
+(setq sly-lisp-implementations
+      '((sbcl ("sbcl") :coding-system utf-8-unix)))
 
 ;;; Rainbow Parentheses
 
