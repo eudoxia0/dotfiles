3e44bf291c184e76322d1fe16954b9d7bb2c573a
Fernando Borretti <eudoxiahp@gmail.com>
2016-08-14 11:10:57 -0300

    Delete stuff from common.sh

--- bootstrap/common.sh
+++ bootstrap/common.sh
@@ -4,10 +4,6 @@
 
 sudo npm -g install jslint
 
-# install useful ruby gems
-gem install veewee
-gem install sass
-
 # Lisp
 curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
 sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
