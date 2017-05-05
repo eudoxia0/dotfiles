b061a1f89f8135ca6ab44654a9622c72bef0de5c
Fernando Borretti <eudoxiahp@gmail.com>
2017-05-05 18:34:00 -0300

    Use StumpWM

--- x11/xsession.sh
+++ x11/xsession.sh
@@ -39,5 +39,5 @@ export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 #export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
 
 xmodmap ~/.Xmodmap
-exec spectrwm
+exec stumpwm
 #exec dbus-launch --exit-with-session dwm
