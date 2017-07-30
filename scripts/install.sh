1bf0e85dc0a0d37829b0adddd460d72e3df05061
Fernando Borretti <eudoxiahp@gmail.com>
2017-07-30 10:07:22 -0300

    Delete diceware script

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,6 +10,5 @@ mkdir $SCRIPTS_DIR
 
 cp backup.sh $SCRIPTS_DIR/backup
 cp cl.sh $SCRIPTS_DIR/cl
-cp diceware.lisp $SCRIPTS_DIR/diceware
 cp rotate-wallpaper.sh $SCRIPTS_DIR/rotate_wallpaper
 cp git-merge-repo.sh $SCRIPTS_DIR/git-merge-repo
