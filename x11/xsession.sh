928e07ea9585f27ac592bc715ee6babfb845aa7f
Fernando Borretti <eudoxiahp@gmail.com>
2018-06-05 00:57:36 -0300

    In .xsession, run xrdb on my .Xresources file

--- x11/xsession.sh
+++ x11/xsession.sh
@@ -30,5 +30,6 @@ export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 #export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
 
 xmodmap ~/.Xmodmap
+xrdb ~/.Xresources
 exec stumpwm
 #exec dbus-launch --exit-with-session dwm
