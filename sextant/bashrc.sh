d88aeac4c04697fb081434ec3e61848ed2b24c4b
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-21 01:31:29 -0300

    Commented out PS1

--- sextant/bashrc.sh
+++ sextant/bashrc.sh
@@ -92,3 +92,5 @@ if ! shopt -oq posix; then
 fi
 
 export PATH="$PATH:$HOME/.scripts"
+
+#PS1="\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\$\[\033[01m\][ \[\033[01;34m\]\u@\h \[\033[00m\]\[\033[01m\]] \[\033[01;32m\]\w\[\033[00m\]\n\[\033[01;34m\]$\[\033[00m\]> "
