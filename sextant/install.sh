25671578ca847f682d87eb24cc8aad2fc9466afd
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-21 18:46:28 -0300

    Update install.sh

--- sextant/install.sh
+++ sextant/install.sh
@@ -4,19 +4,20 @@ sudo apt-get update
 
 # X11
 sudo apt-get install -y xserver-xorg-core xserver-xorg-video-intel \
-     xserver-xorg-input-evdev x11-xserver-utils xinit xdm xterm ctwm
+    xserver-xorg-input-evdev x11-xserver-utils xinit xdm xterm xdotool xcape \
+    stumpwm
 
 # Sound
 sudo apt-get install -y pavucontrol pulseaudio pulseaudio-module-zeroconf \
-     alsa-utils avahi-daemon
+    alsa-utils avahi-daemon
 # stochastically experiment with alsamixer and pavucontrol until it works
 
 # Packages
-
 sudo apt-get install -y git make emacs25 duplicity gpg sqlite3 libsqlite3-dev \
     texlive-xetex firefox-esr xcape neofetch fonts-inconsolata xfonts-terminus \
-    ttf-linux-libertine clang mupdf acpi viewnior lxappearance ntp \
-    xscreensaver xscreensaver-data-extra xscreensaver-gl xscreensaver-gl-extra
+    ttf-linux-libertine clang mupdf acpi viewnior lxappearance ntp feh gnuplot \
+    xscreensaver xscreensaver-data-extra xscreensaver-gl xscreensaver-gl-extra \
+    git transmission-gtk
 
 # Java
 sudo apt-get install -y openjdk-8-jdk maven
@@ -35,16 +36,15 @@ if ! [ -x "$(command -v stack)" ]; then
 fi
 
 # SML
-sudo apt-get install -y git mlton smlnj
+sudo apt-get install -y mlton smlnj
 
 # Config files and scripts
-
 cp gitconfig.txt ~/.gitconfig
 cp psqlrc.txt ~/.psqlrc
 cp xscreensaver.txt ~/.xscreensaver
 cp gtkrc.txt ~/.gtkrc-2.0
-cp ctwmrc.txt ~/.ctwmrc
 cp bashrc.sh ~/.bashrc
+cp stumpwmrc.lisp ~/.stumpwmrc
 
 mkdir -p ~/.scripts
 cp battery.sh ~/.scripts/battery.sh
@@ -61,3 +61,7 @@ sudo cp xdm-xsetup /etc/X11/xdm/Xsetup
 sudo cp xdm-xstartup /etc/X11/xdm/Xstartup
 sudo chmod +x /etc/X11/xdm/Xsetup
 sudo chmod +x /etc/X11/xdm/Xstartup
+
+# Wallpapers
+mkdir -p ~/.walls
+cp -R wallpapers/. ~/.walls
