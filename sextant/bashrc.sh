7239ea545730f32d4519085dd93d4d767a6d0b1c
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-21 01:19:55 -0300

    Use colors in grep

--- sextant/bashrc.sh
+++ sextant/bashrc.sh
@@ -75,13 +75,9 @@ esac
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto -1'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
 
-    #alias grep='grep --color=auto'
-    #alias fgrep='fgrep --color=auto'
-    #alias egrep='egrep --color=auto'
+    alias ls='ls --color=auto -1'
+    alias grep='grep --color=auto'
 fi
 
 # colored GCC warnings and errors
