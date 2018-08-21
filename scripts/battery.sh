abd0e6682183011b2059751b1c0d9a2f779a7d75
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-20 23:02:40 -0300

    Add a script to notify me when the battery is low

---
+++ scripts/battery.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function probe_battery() {
+    local var charge=`acpi -b | grep -oP "\d+%" | tr -d "%"`
+    if (( charge < 10 )); then
+	xmessage -center "Battery level: $charge%"
+    fi
+}
+
+while true; do
+    probe_battery;
+    sleep 300
+done
