2e42aa64349d9cf03ae6a2603da17ae7058cd731
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-21 18:46:15 -0300

    Set wallpaper and exec stumpwm

--- sextant/xsession.sh
+++ sextant/xsession.sh
@@ -3,5 +3,6 @@ xmodmap ~/.Xmodmap
 xcape -e "Shift_L=parenleft;Shift_R=parenright"
 xscreensaver -no-splash &
 .scripts/battery.sh &
+feh --bg-scale ~/.walls/view-of-dresden-by-moonlight-johan-christian-dahl.jpg
 
-exec ctwm
+exec stumpwm
