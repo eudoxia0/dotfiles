4e862cfb4337a4ad9113b8db635472d4abc3c779
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-23 20:23:32 -0300

    Move emacs install script to sextant/install.sh

--- sextant/install.sh
+++ sextant/install.sh
@@ -69,3 +69,19 @@ cp -R wallpapers/. ~/.walls
 
 # Uninstall
 sudo apt-get -y remove bluetooth
+
+# Emacs
+EMACS_DIR=~/.emacs.d
+
+if [ -d "$EMACS_DIR" ]; then
+  rm -rf $EMACS_DIR
+fi
+
+mkdir $EMACS_DIR
+mkdir $EMACS_DIR/lang
+
+for file in init config file-modes keys packages theme lang/lisp \
+            lang/haskell lang/lass lang/coq lang/jsx scriba
+do
+  cp emacs/$file.el $EMACS_DIR/$file.el
+done
