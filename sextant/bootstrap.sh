fd13bdb84c23ffdb4b534a86a835527ee97d06e8
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-24 19:30:32 -0300

    Simplify timezone config process

--- sextant/bootstrap.sh
+++ sextant/bootstrap.sh
@@ -1,8 +1,7 @@
 # Run as root immediately after installation
 
-dpkg-reconfigure tzdata
-# None of the above > GMT+3
-# REMEMBER: THE TIMEZONES ARE MISLABELED, GMT+3 IS UTC-3
+echo "Etc/GMT+3" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 apt-get update
 apt-get install curl sudo
 echo "eudoxia ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/eudoxia
