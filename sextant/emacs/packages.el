cf722c5939536a0a1556b75e3ab7e7af5aebad6b
Fernando Borretti <eudoxiahp@gmail.com>
2018-08-28 20:02:26 -0300

    Reorganize packages

--- sextant/emacs/packages.el
+++ sextant/emacs/packages.el
@@ -6,15 +6,17 @@
 (package-initialize)
 
 (defvar my-packages
-  '(;; Major modes
-    sly yaml-mode d-mode markdown-mode
+  '(;; File-specific modes
+    yaml-mode d-mode markdown-mode
     gnuplot-mode haskell-mode scss-mode
     rainbow-mode web-mode projectile grizzl
-    graphviz-dot-mode powerline rust-mode tuareg adoc-mode ;pov-mode idris-mode
-    sml-mode disable-mouse
-    ;; Other plugins
-    rainbow-delimiters ecb auto-complete ag highlight-indentation
-    flycheck ido flyspell paredit company unicode-fonts
+    graphviz-dot-mode powerline rust-mode tuareg adoc-mode
+    sml-mode
+    ;; Lisp
+    sly
+    ;; Minor modes and tools
+    rainbow-delimiters auto-complete ag highlight-indentation
+    flycheck ido flyspell paredit company unicode-fonts disable-mouse
     ;; Themes
     sublime-themes
     ))
