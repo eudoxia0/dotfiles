2d24aa14ea9e53a96c186a8b46c11188b5c7c4bf
Fernando Borretti <eudoxiahp@gmail.com>
2018-09-13 21:30:15 -0300

    Longer bash history

--- sextant/bashrc.sh
+++ sextant/bashrc.sh
@@ -17,7 +17,7 @@ shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
-HISTFILESIZE=2000
+HISTFILESIZE=20000
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
