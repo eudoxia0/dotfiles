2d5484c0b63b44d09d66c19bbb9955b57ff95d7b
Fernando Borretti <eudoxiahp@gmail.com>
2018-12-31 01:18:20 -0300

    remove

--- sextant/xsession.sh
+++ sextant/xsession.sh
@@ -3,6 +3,5 @@ xrdb ~/.Xresources
 xcape -e "Shift_L=parenleft;Shift_R=parenright"
 xscreensaver -no-splash &
 .scripts/battery.sh &
-feh --bg-scale ~/.walls/view-of-dresden-by-moonlight-johan-christian-dahl.jpg
 
 exec stumpwm
