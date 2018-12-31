84bb85370e8db5e709b40f369066825447e7623e
Fernando Borretti <eudoxiahp@gmail.com>
2018-12-31 01:18:59 -0300

    update install.sh

--- sextant/install.sh
+++ sextant/install.sh
@@ -51,7 +51,7 @@ cp psqlrc.txt ~/.psqlrc
 cp xscreensaver.txt ~/.xscreensaver
 cp gtkrc.txt ~/.gtkrc-2.0
 cp bashrc.sh ~/.bashrc
-cp ratpoisonrc.txt ~/.ratpoisonrc
+cp ctwmrc.txt ~/.ctwmrc
 sudo cp dhclient.txt /etc/dhcp/dhclient.conf
 
 mkdir -p ~/.scripts
