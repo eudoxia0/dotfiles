0a0788e64e983814db0c5672cb3f2084aae7c621
Fernando Borretti <eudoxiahp@gmail.com>
2019-01-16 23:19:34 -0300

    Set GOPATH

--- sextant/bashrc.sh
+++ sextant/bashrc.sh
@@ -98,4 +98,6 @@ export PATH="$PATH:~/.npm-global/bin"
 export PATH="$PATH:/usr/lib/dart/bin"
 export PATH="$PATH:~/.pub-cache/bin"
 
+export GOPATH=$HOME/code/go
+
 #PS1="\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\$\[\033[01m\][ \[\033[01;34m\]\u@\h \[\033[00m\]\[\033[01m\]] \[\033[01;32m\]\w\[\033[00m\]\n\[\033[01;34m\]$\[\033[00m\]> "
