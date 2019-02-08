f22389baf6b2ca5477ce1c1044b9fe02d1275898
Fernando Borretti <eudoxiahp@gmail.com>
2019-02-07 23:09:14 -0300

    Update xsession

--- sextant/xsession.sh
+++ sextant/xsession.sh
@@ -1,8 +1,8 @@
-xmodmap ~/.Xmodmap
-xrdb ~/.Xresources
-xcape -e "Shift_L=parenleft;Shift_R=parenright"
+.scripts/rotate_wallpaper.sh &
+xmodmap ~/.Xmodmap &
+xrdb ~/.Xresources &
+xcape -e "Shift_L=parenleft;Shift_R=parenright" &
 xscreensaver -no-splash &
 .scripts/battery.sh &
-xsetroot -solid "gray40"
 
-exec fvwm
+exec stumpwm
