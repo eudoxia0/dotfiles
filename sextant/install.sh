0c6b8159b95a38b98747cd195ad3d236b156be06
Fernando Borretti <eudoxiahp@gmail.com>
2019-06-16 18:39:59 -0400

    Install sunclock

--- sextant/install.sh
+++ sextant/install.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 
 X11_PACKAGES="xserver-xorg-core xserver-xorg-video-intel \
 xserver-xorg-input-evdev x11-xserver-utils xinit xdm xterm xdotool xcape \
-stumpwm"
+stumpwm sunclock"
 
 SOUND_PACKAGES="pavucontrol pulseaudio pulseaudio-module-zeroconf alsa-utils \
 avahi-daemon"
