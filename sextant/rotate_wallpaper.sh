eeae9496496155945e32f37d4b7c069fa4922415
Fernando Borretti <eudoxiahp@gmail.com>
2019-06-16 19:12:04 -0400

    Update rotate_wallpaper.sh script

--- sextant/rotate_wallpaper.sh
+++ sextant/rotate_wallpaper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 while true; do
-  feh --randomize --bg-fill ~/images/wallpapers/rotation/*
+  feh --randomize --bg-fill ~/files/images/wallpapers/rotation/*
   sleep 300
 done
