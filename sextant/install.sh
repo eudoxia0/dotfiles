7f87ba7bba78873d8950d06aaef590c71fea5d05
Fernando Borretti <eudoxiahp@gmail.com>
2019-06-16 18:45:38 -0400

    Don't install ttf-mscorefonts-installer

--- sextant/install.sh
+++ sextant/install.sh
@@ -24,7 +24,7 @@ fonts-linuxlibertine clang mupdf acpi viewnior lxappearance ntp feh gnuplot \
 xscreensaver xscreensaver-data-extra xscreensaver-gl xscreensaver-gl-extra \
 transmission-gtk djview4 easytag mpv unrar-free ttf-ancient-fonts \
 libwebkit2gtk-4.0-37-gtk2 ocaml ocaml-native-compilers docbook-xml docbook-xsl \
-docbook-xsl-ns xsltproc fop lm-sensors tlp thermald mupdf-tools ttf-mscorefonts-installer \
+docbook-xsl-ns xsltproc fop lm-sensors tlp thermald mupdf-tools \
 zotero-standalone clementine pcmanfm screenfetch leafpad firmware-iwlwifi keepassx calibre xarchiver htop ansible inxi"
 
 RUBY_PACKAGES="rbenv ruby-build"
