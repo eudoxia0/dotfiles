8e71614775c7dc6a2ccee40ac5eea1057bea5b5e
Fernando Borretti <eudoxiahp@gmail.com>
2019-06-16 19:11:43 -0400

    Note to self

--- sextant/xsession.sh
+++ sextant/xsession.sh
@@ -5,4 +5,7 @@ xcape -e "Shift_L=parenleft;Shift_R=parenright" &
 xscreensaver -no-splash &
 .scripts/battery.sh &
 
+# NOTE: this is how you shut down the backlight on the ASUS laptop
+# sudo bash -c 'echo 0 > /sys/devices/platform/asus-nb-wmi/leds/asus\:\:kbd_backlight/brightness'
+
 exec stumpwm
