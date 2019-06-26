937a498ab47e71ef9ab005f0ed1028c81655db7f
Fernando Borretti <eudoxiahp@gmail.com>
2019-06-25 22:51:45 -0300

    Change wm to fvwm

--- sextant/xsession.sh
+++ sextant/xsession.sh
@@ -9,4 +9,4 @@ redshift &
 # NOTE: this is how you shut down the backlight on the ASUS laptop
 # sudo bash -c 'echo 0 > /sys/devices/platform/asus-nb-wmi/leds/asus\:\:kbd_backlight/brightness'
 
-exec stumpwm
+exec fvwm
