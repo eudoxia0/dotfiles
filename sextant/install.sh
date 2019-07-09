57be3c634277e71b36605c4f03cf85eb1ed80538
Fernando Borretti <eudoxiahp@gmail.com>
2019-07-08 23:58:11 -0300

    Remove rbenv commands

--- sextant/install.sh
+++ sextant/install.sh
@@ -33,13 +33,6 @@ zotero-standalone clementine pcmanfm screenfetch keepassx calibre xarchiver htop
 sudo apt-get install -y $X11_PACKAGES $SOUND_PACKAGES $DEV_PACKAGES \
      $GENERAL_PACKAGES
 
-# Ruby
-if ! [ -x "$(command -v rbenv)" ]; then
-    rbenv install 2.4.0
-    rbenv global 2.4.0
-    rbenv exec gem install jekyll
-fi
-
 # Haskell
 if ! [ -x "$(command -v stack)" ]; then
     curl -sSL https://get.haskellstack.org/ | sh
