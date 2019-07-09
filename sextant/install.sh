9b9cc30b5d08bc138f292a7f0b0c86dbf5818fb6
Fernando Borretti <eudoxiahp@gmail.com>
2019-07-09 00:37:12 -0300

    Copy stumpwmrc to .config/stumpwm

--- sextant/install.sh
+++ sextant/install.sh
@@ -40,7 +40,8 @@ cp psqlrc.txt ~/.psqlrc
 cp xscreensaver.txt ~/.xscreensaver
 cp gtkrc.txt ~/.gtkrc-2.0
 cp bashrc.sh ~/.bashrc
-cp stumpwmrc.lisp ~/.stumpwmrc
+mkdir -p ~/.config/stumpwm/
+cp stumpwmrc.lisp ~/.config/stumpwm/config
 sudo cp dhclient.txt /etc/dhcp/dhclient.conf
 
 mkdir -p ~/.scripts
