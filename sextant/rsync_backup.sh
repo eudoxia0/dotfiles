4d3e9d8aac9561ebb5dd4ef6aeba9ee81414f08a
Fernando Borretti <eudoxiahp@gmail.com>
2019-07-16 23:27:34 -0300

    Write rsync_backup script

--- sextant/rsync_backup.sh
+++ sextant/rsync_backup.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
+echo "Drive: (probably /media/eudoxia/BACKUP)"
+read -s BACKUP_DRIVE
+
+rsync --quiet --archive --human-readable --perms --executability --times --checksum --delete \
+      ~/files/ \
+      $BACKUP_DRIVE/files
