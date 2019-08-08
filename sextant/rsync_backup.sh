61c1561e43fcca82daca8b42950234bf2e721280
Fernando Borretti <fernando@borretti.me>
2019-08-08 20:09:37 -0300

    Update rsync_backup.sh

--- sextant/rsync_backup.sh
+++ sextant/rsync_backup.sh
@@ -3,6 +3,4 @@
 set -euxo pipefail
 BACKUP_DRIVE=/media/eudoxia/Backup
 
-rsync --archive --human-readable --perms --executability --times --checksum --delete \
-      ~/files/ \
-      $BACKUP_DRIVE/files
+rsync --archive --human-readable --perms --executability --times --checksum --delete ~/files/ $BACKUP_DRIVE/files
