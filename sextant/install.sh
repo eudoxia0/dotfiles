b2d89255486583de8589593cb285b068783444c5
Fernando Borretti <fernando@borretti.me>
2022-03-24 13:10:50 +0000

    Update xresources for st

--- sextant/install.sh
+++ sextant/install.sh
@@ -61,7 +61,6 @@ cp xcompose.txt ~/.XCompose
 cp xmodmap.txt ~/.config/x11/xmodmap
 cp xsession.sh ~/.xsession
 cp xresources.txt ~/.config/x11/xresources
-cp xresources-basic-theme.txt ~/.config/x11/basic-theme
 
 sudo cp xdm-xsetup /etc/X11/xdm/Xsetup
 sudo cp xdm-xstartup /etc/X11/xdm/Xstartup
