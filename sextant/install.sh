a0ef0070430727b44f7807d318a6572d018281b3
Fernando Borretti <fernando@borretti.me>
2022-11-20 09:33:48 +0000

    Move xmodmap and xresources config

--- sextant/install.sh
+++ sextant/install.sh
@@ -6,7 +6,3 @@ cp bashrc.sh ~/.bashrc
 
 mkdir -p ~/.config/spectrwm/
 cp spectrwm.conf ~/.config/spectrwm/spectrwm.conf
-
-mkdir -p ~/.config/x11/
-cp xmodmap.txt ~/.config/x11/xmodmap
-cp xresources.txt ~/.config/x11/xresources
