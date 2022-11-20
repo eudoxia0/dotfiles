01638d1e7104edd8c47f7066b36c715530282374
Fernando Borretti <fernando@borretti.me>
2022-11-20 09:30:19 +0000

    Prune install.sh

--- sextant/install.sh
+++ sextant/install.sh
@@ -12,16 +12,6 @@ cp spectrwm.conf ~/.config/spectrwm/spectrwm.conf
 mkdir -p ~/.emacs.d/
 cp init.el ~/.emacs.d/init.el
 
-mkdir -p ~/.scripts
-cp battery.sh ~/.scripts/battery.sh
-cp backup.sh ~/.scripts/backup.sh
-cp embed_fonts.sh ~/.scripts/embed_fonts.sh
-cp rotate_wallpaper.sh ~/.scripts/rotate_wallpaper.sh
-chmod +x ~/.scripts/battery.sh
-chmod +x ~/.scripts/backup.sh
-chmod +x ~/.scripts/embed_fonts.sh
-chmod +x ~/.scripts/rotate_wallpaper.sh
-
 mkdir -p ~/.config/x11/
 cp xcompose.txt ~/.XCompose
 cp xmodmap.txt ~/.config/x11/xmodmap
