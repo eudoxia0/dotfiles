8fd2414823954e60963aeae9638a9582bc292040
Fernando Borretti <fernando@borretti.me>
2022-11-20 09:23:48 +0000

    Prune install.sh further

--- sextant/install.sh
+++ sextant/install.sh
@@ -1,15 +1,14 @@
 # Config files and scripts
 mkdir -p ~/.config/git/
 cp gitconfig.txt ~/.config/git/config
-cp psqlrc.txt ~/.psqlrc
+
 cp xscreensaver.txt ~/.xscreensaver
 cp gtkrc.txt ~/.gtkrc-2.0
 cp bashrc.sh ~/.bashrc
-mkdir -p ~/.config/stumpwm/
-cp stumpwmrc.lisp ~/.config/stumpwm/config
+
 mkdir -p ~/.config/spectrwm/
 cp spectrwm.conf ~/.config/spectrwm/spectrwm.conf
-sudo cp dhclient.txt /etc/dhcp/dhclient.conf
+
 mkdir -p ~/.emacs.d/
 cp init.el ~/.emacs.d/init.el
 
@@ -28,10 +27,3 @@ cp xcompose.txt ~/.XCompose
 cp xmodmap.txt ~/.config/x11/xmodmap
 cp xsession.sh ~/.xsession
 cp xresources.txt ~/.config/x11/xresources
-
-sudo cp xdm-xsetup /etc/X11/xdm/Xsetup
-sudo cp xdm-xstartup /etc/X11/xdm/Xstartup
-sudo chmod +x /etc/X11/xdm/Xsetup
-sudo chmod +x /etc/X11/xdm/Xstartup
-
-cp mimeapps.list ~/.local/share/applications/mimeapps.list
