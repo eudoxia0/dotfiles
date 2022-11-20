3008e395b67e80eb8feb095ae651106002adb5b9
Fernando Borretti <fernando@borretti.me>
2022-11-20 09:30:02 +0000

    Delete old script

--- sextant/.scripts/rsync_backup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-BACKUP_DRIVE=/media/eudoxia/Backup
-
-rsync --progress --archive --human-readable --perms --executability --times --checksum --delete ~/files/ $BACKUP_DRIVE/files
