7362aaf31a2684df07f29788264eb61b9c669fd2
Fernando Borretti <fernando@borretti.me>
2022-11-20 20:55:57 +1100

    Add scripts to apply and unapply configurations

---
+++ apply.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Apply a configuration
+
+stow --target=/home/eudoxia --stow $1
---
+++ unapply.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Remove a configuration
+
+stow --target=/home/eudoxia --unstow $1
