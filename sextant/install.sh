8e7c3e7843422de7a323a43e1edff0c7a6bcd4e0
Fernando Borretti <fernando@borretti.me>
2022-11-20 09:31:29 +0000

    Move Emacs config

--- sextant/install.sh
+++ sextant/install.sh
@@ -7,9 +7,6 @@ cp bashrc.sh ~/.bashrc
 mkdir -p ~/.config/spectrwm/
 cp spectrwm.conf ~/.config/spectrwm/spectrwm.conf
 
-mkdir -p ~/.emacs.d/
-cp init.el ~/.emacs.d/init.el
-
 mkdir -p ~/.config/x11/
 cp xcompose.txt ~/.XCompose
 cp xmodmap.txt ~/.config/x11/xmodmap
