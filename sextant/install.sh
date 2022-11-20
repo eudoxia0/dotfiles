6ff83d04fa2749851380c105593d6cd0e6c5c7fa
Fernando Borretti <fernando@borretti.me>
2022-11-20 09:32:44 +0000

    Move xsession config

--- sextant/install.sh
+++ sextant/install.sh
@@ -10,5 +10,4 @@ cp spectrwm.conf ~/.config/spectrwm/spectrwm.conf
 mkdir -p ~/.config/x11/
 cp xcompose.txt ~/.XCompose
 cp xmodmap.txt ~/.config/x11/xmodmap
-cp xsession.sh ~/.xsession
 cp xresources.txt ~/.config/x11/xresources
