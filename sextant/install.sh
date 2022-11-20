94bc1a0c5e8a0eb3c0984458cffccc22b2b95aa6
Fernando Borretti <fernando@borretti.me>
2022-11-20 09:33:04 +0000

    Move XCompose config

--- sextant/install.sh
+++ sextant/install.sh
@@ -8,6 +8,5 @@ mkdir -p ~/.config/spectrwm/
 cp spectrwm.conf ~/.config/spectrwm/spectrwm.conf
 
 mkdir -p ~/.config/x11/
-cp xcompose.txt ~/.XCompose
 cp xmodmap.txt ~/.config/x11/xmodmap
 cp xresources.txt ~/.config/x11/xresources
