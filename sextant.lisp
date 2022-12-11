8c71ad73f495365e8a6b43c56a5336ba7208a5eb
Fernando Borretti <fernando@borretti.me>
2022-12-11 15:54:23 +1100

    Add sextant.lisp

---
+++ sextant.lisp
@@ -0,0 +1,50 @@
+;;;;
+;;;; Configuration for host "sextant"
+;;;;
+(in-package :cl-user)
+(defpackage sextant
+  (:use :cl :lcm :common)
+  (:export :sextant))
+(in-package :sextant)
+
+;;; Constants.
+
+(defparameter +packages+
+  (list
+   "xterm"
+   "git"
+   "rclone"
+   "emacs"
+   "keepassx"
+   "spectrwm"
+   "make"
+   "gcc"
+   "pcmanfm"
+   "calibre"
+   "scrot"
+   "xfonts-terminus"
+   "xcape"
+   "xsecurelock"
+   "dmenu"
+   "rxvt-unicode"
+   "acpi"
+   "lxrandr"
+   "nvidia-prime"
+   "fonts-inconsolata"
+   "sbcl"
+   "curl"
+   "pavucontrol"
+   "whois"
+   "expect"
+   ))
+
+;;; Configuration class.
+
+(defconfig sextant
+  :secrets ()
+  :components (((config sextant) vault)
+               (declare (ignore config vault))
+               (list
+                (make-instance 'apt-one-way-component
+                               :title "Install apt packages"
+                               :packages +packages+))))
