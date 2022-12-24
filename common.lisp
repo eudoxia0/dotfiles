11080976cfa1f5645d0f5c3162bbc829180a44de
Fernando Borretti <fernando@borretti.me>
2022-12-24 11:34:24 +1100

    Use the builtin file component from the latest version of LCM

--- common.lisp
+++ common.lisp
@@ -3,10 +3,7 @@
 ;;;;
 (in-package :cl-user)
 (defpackage common
-  (:use :cl :lcm)
-  (:import-from :org.shirakumo.file-attributes
-                :attributes
-                :encode-attributes)
+  (:use :cl :lcm :lcm.builtin)
   (:export :apt-one-way-component
            :make-file-component
            :native-path))
@@ -43,66 +40,6 @@
 ;;;
 ;;; File Component
 ;;;
-
-(defun chmod+x (pathname)
-  (let ((attrs (list :other-execute nil
-                     :other-write nil
-                     :other-read t
-                     :group-execute nil
-                     :group-write t
-                     :group-read t
-                     :owner-execute t ;; here
-                     :owner-write t
-                     :owner-read t
-                     :sticky nil
-                     :set-group nil
-                     :set-user nil
-                     :fifo nil
-                     :device nil
-                     :directory nil
-                     :normal t
-                     :link nil
-                     :socket nil)))
-    (setf (attributes pathname) (encode-attributes attrs))))
-
-(defcomponent file-component (component)
-  ((path :reader component-path
-         :initarg :path
-         :type pathname
-         :documentation "The path to the file.")
-   (contents :reader component-contents
-             :initarg :contents
-             :type string
-             :documentation "The file contents.")
-   (executable :reader component-executable
-               :initarg :executable
-               :initform nil
-               :type boolean
-               :documentation "Whether the resulting file should be executable."))
-  :documentation "A component to create a file."
-
-  :appliedp (((component file-component))
-             "Applied when the file exists and its contents are identical to the string."
-             (with-slots (path contents) component
-               (and (probe-file path)
-                    (string= (uiop:read-file-string path) contents))))
-
-  :apply (((component file-component))
-          (with-slots (path contents executable) component
-            ;; Ensure the parent directories exist.
-            (ensure-directories-exist (uiop:pathname-directory-pathname path))
-            ;; Write the file.
-            (with-open-file (stream path
-                                    :direction :output
-                                    :if-exists :supersede
-                                    :if-does-not-exist :create)
-              (write-string contents stream))
-            ;; chmod+x if needd
-            (when executable
-              (chmod+x path))))
-
-  :unapply (((component file-component))
-            (delete-file (component-path component))))
 
 (defun make-file-component (&key title source target (executable nil))
   (make-instance 'file-component
