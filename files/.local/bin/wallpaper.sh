df3b7b170e77a548b4d5c2f229d8891f7eb0dfc6
Fernando Borretti <fernando@borretti.me>
2022-12-29 21:07:21 +1100

    Update the wallpaper script to use a background colour of the wallpapers directory doesn't exist

--- files/.local/bin/wallpaper.sh
+++ files/.local/bin/wallpaper.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-while true; do
-  feh --randomize --bg-fill ~/files/Images/Wallpapers/rotation/*
-  sleep 300
-done
+if [ -d ~/files/Images/Wallpapers/ ]; then
+    # If the wallpapers directory exists, choose a random wallpaper and switch
+    # every 5 minutes.
+    feh --randomize --bg-fill ~/files/Images/Wallpapers/rotation/*
+    sleep 300
+else
+    # Otherwise set a default colour.
+    xsetroot -solid '#708090'
+fi
