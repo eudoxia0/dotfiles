783b6f2a7b61340241672d87f5f6ae3b3a7de853
Fernando Borretti <fernando@borretti.me>
2023-03-04 15:41:47 +1100

    fix wallpaper.sh

--- files/.local/bin/wallpaper.sh
+++ files/.local/bin/wallpaper.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -d ~/files/Images/Wallpapers/ ]; then
+if [ -d ~/files/4\ Resources/6\ Images/wallpapers/rotation ]; then
     # If the wallpapers directory exists, choose a random wallpaper and switch
     # every 5 minutes.
-    feh --randomize --bg-fill ~/files/Images/Wallpapers/rotation/*
-    sleep 300
+    while true; do
+        feh --randomize --bg-fill ~/files/4\ Resources/6\ Images/wallpapers/rotation/*
+        sleep 300
+    done
 else
     # Otherwise set a default colour.
     xsetroot -solid '#555577'
