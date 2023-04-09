333cc25b8ef99a23af481e0ffc7ff60327a8e4b3
Fernando Borretti <fernando@borretti.me>
2023-04-09 16:59:19 +1000

    fix recorder.sh

--- files/.local/bin/recorder.sh
+++ files/.local/bin/recorder.sh
@@ -10,14 +10,15 @@ fi
 # Main loop
 while true; do
     # Get the current timestamp
-    TIMESTAMP=$(date "+%Y/%m/%d/%H-%M-%S")
+    DATE=$(date "+%Y/%m/%d")
+    TIME=$(date "+%H-%M-%S")
 
-    # Create the directory structure
-    DIR_PATH="$HOME/files/4 Resources/6 Images/logs/$TIMESTAMP"
+    # Create the directory structure down to the day
+    DIR_PATH="$HOME/files/4 Resources/6 Images/logs/$DATE"
     mkdir -p "$DIR_PATH"
 
-    # Take a screenshot and save it to the specified path
-    FILE_PATH="$DIR_PATH.png"
+    # Take a screenshot and save it to the specified path with H-M-S.png format
+    FILE_PATH="$DIR_PATH/$TIME.png"
     scrot "$FILE_PATH"
 
     # Wait 60 seconds
