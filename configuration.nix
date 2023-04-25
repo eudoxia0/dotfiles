5592ad3fd656b8ab4f60369231f4aa7534bed9d7
Fernando Borretti <fernando@borretti.me>
2023-04-25 15:41:56 +1000

    Explicitly enable the firewall

--- configuration.nix
+++ configuration.nix
@@ -182,8 +182,7 @@
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
