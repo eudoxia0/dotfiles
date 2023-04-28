33d69969b43cf70428ae0bf46adda2ec18b437c2
Fernando Borretti <fernando@borretti.me>
2023-04-29 07:20:30 +1000

    remove comments

--- configuration.nix
+++ configuration.nix
@@ -92,12 +92,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
