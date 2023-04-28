850c30ec1b12c3f0d295661295072c5572eb8fc3
Fernando Borretti <fernando@borretti.me>
2023-04-29 07:20:57 +1000

    trim comments

--- configuration.nix
+++ configuration.nix
@@ -151,12 +151,8 @@
     ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  ];
+  # System packages.
+  environment.systemPackages = with pkgs; [];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
