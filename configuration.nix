ab492ad0977e802b025826f6a68a43288064ddda
Fernando Borretti <fernando@borretti.me>
2023-04-29 14:24:51 +1000

    Merge home.nix into configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,17 @@
 { config, pkgs, ... }:
 
+let
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz";
+    sha256 = "1cp2rpprcfl4mjsrsrpfg6278nf05a0mpl3m0snksvdalfmc5si5";
+  };
+in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./home.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -178,6 +184,44 @@
 
   # Disable Bluetooth.
   hardware.bluetooth.enable = false;
+
+  home-manager.users.eudoxia = {
+    home.stateVersion = "22.11";
+
+    home.file = {
+      ".bashrc" = {
+        source = ./sources/bashrc.sh;
+      };
+      ".config/spectrwm/spectrwm.conf" = {
+        source = ./sources/spectrwm.conf;
+      };
+      ".xprofile" = {
+        source = ./sources/xprofile.sh;
+      };
+      ".garglkrc" = {
+        source = ./sources/garglkrc.conf;
+      };
+      ".XCompose" = {
+        source = ./sources/XCompose;
+      };
+      ".emacs.d/init.el" = {
+        source = ./sources/init.el;
+      };
+      ".config/git/config" = {
+        source = ./sources/gitconfig.conf;
+      };
+      ".xmodmap" = {
+        source = ./sources/xmodmap;
+      };
+      ".Xresources" = {
+        source = ./sources/xresources;
+      };
+      ".local/bin" = {
+        source = ./sources/scripts;
+        recursive = true;
+      };
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
