780502996eac51b022e83c0025fb81efc17f2e3b
Fernando Borretti <fernando@borretti.me>
2023-04-29 14:53:12 +1000

    update recrank script

--- recrank.sh
+++ recrank.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
-# Apply the current configuration.
-sudo nixos-rebuild switch -I nixos-config=sextant.nix
+function recrank() {
+    sudo nixos-rebuild switch -I nixos-config=$1.nix
+}
+
+if [ "$hostname" == "sextant" ]; then
+    recrank sextant
+elif [ "$hostname" == "bullroarer" ]; then
+    recrank bullroarer
+else
+    if [ -z "$1" ]; then
+	echo "unknown hostname"
+	exit 1
+    else
+	recrank $1
+    fi
+fi
