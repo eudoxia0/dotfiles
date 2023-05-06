144e491cf29c64e6db3537d306d42f21a39f6ed4
Fernando Borretti <fernando@borretti.me>
2023-05-06 14:14:54 +1000

    move sound

--- root.nix
+++ root.nix
@@ -75,6 +75,16 @@ in
   # Sound
   #
 
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   #
   # X11
   #
@@ -137,16 +147,6 @@ in
   services.printing.enable = true;
 
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
