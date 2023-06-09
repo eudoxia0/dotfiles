48f3b076bf8afb25ebd6534c9090d7fbe5a4e872
Fernando Borretti <fernando@borretti.me>
2023-06-10 09:39:10 +1000

    emacs: add a function to input special Unicode characters

--- sources/init.el
+++ sources/init.el
@@ -215,6 +215,25 @@
   (interactive)
   (set-input-method "farsi-transliterate-banan"))
 
+(defvar special-inputs
+  '(;; Punctuation
+    ("--" . "–")
+    ("---" . "—")
+    ("..." . "…")
+    ;; Spanish
+    ("n~" . "ñ"))
+  "Map of shorthand to special characters.")
+
+(defun e/special ()
+  "Enter special characters."
+  (interactive)
+  (let* ((completions (mapcar #'car special-inputs))
+         (input (completing-read "e/special: " completions))n
+         (output (cdr (assoc input special-inputs))))
+    (if output
+        (insert output)
+        (message "Not a shorthand: %s" input))))
+
 ;;;;
 ;;;; Interaction
 ;;;;
