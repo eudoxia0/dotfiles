ee47babbcac06759674b199013cdeabaa9420d20
Fernando Borretti <fernando@borretti.me>
2023-07-08 14:00:25 +1000

    better script

--- sources/scripts/markdown-toc.py
+++ sources/scripts/markdown-toc.py
@@ -5,13 +5,14 @@ import sys
 
 with open(sys.argv[1], 'r') as file:
     lines = file.readlines()
+    assert len(lines) > 0
 
-pattern = re.compile(r'^(#{1,6}) (.*?) {#(.*?)}$')
+    pattern = re.compile(r'^(#{1,6}) (.*?) {#(.*?)}$')
 
-for line in lines:
-    match = pattern.match(line)
-    if match:
-        level: int = len(match.group(1))
-        name: str = match.group(2)
-        link: str = match.group(3)
-        print(f'{"    " * (level - 1)}1. [{name}](#{link})')
+    for line in lines:
+        match = pattern.match(line)
+        if match:
+            level: int = len(match.group(1))
+            name: str = match.group(2)
+            link: str = match.group(3)
+            print(f'{"    " * (level - 1)}1. [{name}](#{link})')
