e909951bed2541fe8c63fe12b1133e25eeaee004
Fernando Borretti <fernando@borretti.me>
2024-03-02 12:45:37 +1100

    aliases

--- macos/zshrc.sh
+++ macos/zshrc.sh
@@ -5,5 +5,11 @@ export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 
 # Git aliases.
 alias gs="git status"
-alias gcam="git commit -a -m"
+alias gits="git status"
 alias gb="git branch"
+alias gco="git checkout"
+alias gcam="git commit -a -m"
+alias gu="git push -u origin HEAD"
+alias gd="git pull origin"
+alias cf="cargo fmt"
+alias ck="cargo check"
