68190e793687c6bcc200736d33017eff5d655655
Fernando Borretti <fernando@borretti.me>
2024-03-03 13:44:08 +1100

    configure global gitignore

--- macos/recrank.sh
+++ macos/recrank.sh
@@ -3,6 +3,7 @@
 cp config/zprofile.sh ~/.zprofile
 cp config/zshrc.sh ~/.zshrc
 cp config/gitconfig.conf ~/.gitconfig
+cp config/gitignore.txt ~/.gitignore
 mkdir -p ~/.local/bin
 cp -a scripts/* ~/.local/bin/
 touch ~/.hushlogin
