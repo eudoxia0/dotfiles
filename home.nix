639362605bf4ad2181494728078d4d4ebbd45c6e
Fernando Borretti <fernando@borretti.me>
2024-06-15 14:17:12 +1000

    recursive

--- home.nix
+++ home.nix
@@ -20,16 +20,10 @@
             ".gitignore".source = ./home/.global-gitignore;
             ".config/zed/settings.json".source = ./home/.config/zed/settings.json;
             ".emacs.d/init.el".source = ./home/.emacs.d/init.el;
-            ".emacs.d/eudoxia/eudoxia-general.el".source = ./home/.emacs.d/eudoxia/eudoxia-general.el;
-            ".emacs.d/eudoxia/eudoxia-lisp.el".source = ./home/.emacs.d/eudoxia/eudoxia-lisp.el;
-            ".emacs.d/eudoxia/eudoxia-markdown.el".source = ./home/.emacs.d/eudoxia/eudoxia-markdown.el;
-            ".emacs.d/eudoxia/eudoxia-mode-line.el".source = ./home/.emacs.d/eudoxia/eudoxia-mode-line.el;
-            ".emacs.d/eudoxia/eudoxia-olivetti.el".source = ./home/.emacs.d/eudoxia/eudoxia-olivetti.el;
-            ".emacs.d/eudoxia/eudoxia-package.el".source = ./home/.emacs.d/eudoxia/eudoxia-package.el;
-            ".emacs.d/eudoxia/eudoxia-splash.el".source = ./home/.emacs.d/eudoxia/eudoxia-splash.el;
-            ".emacs.d/eudoxia/eudoxia-ui.el".source = ./home/.emacs.d/eudoxia/eudoxia-ui.el;
-            ".emacs.d/eudoxia/eudoxia-xml.el".source = ./home/.emacs.d/eudoxia/eudoxia-xml.el;
-            ".emacs.d/eudoxia/eudoxia.el".source = ./home/.emacs.d/eudoxia/eudoxia.el;
+            ".emacs.d/eudoxia/" = {
+                source = ./home/.emacs.d/eudoxia;
+                recursive = true;
+            };
             ".hushlogin".text = "";
             ".local/bin/backup.sh".source = ./home/.local/bin/backup.sh;
             ".local/bin/canonicalize-screenshots.py".source = ./home/.local/bin/canonicalize-screenshots.py;
