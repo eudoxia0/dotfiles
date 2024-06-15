56fbf1a3a5e20382d10bc97b2cd4e8065533a39d
Fernando Borretti <fernando@borretti.me>
2024-06-15 14:19:50 +1000

    eudoxia-special.el

---
+++ home/.emacs.d/eudoxia/eudoxia-special.el
@@ -0,0 +1,20 @@
+(defvar special-inputs
+  '(;; Punctuation
+    ("--" . "–")
+    ("---" . "—")
+    ("..." . "…")
+    ;; Spanish
+    ("n~" . "ñ"))
+  "Map of shorthand to special characters.")
+
+(defun eudoxia-special ()
+  "Enter special characters."
+  (interactive)
+  (let* ((completions (mapcar #'car special-inputs))
+         (input (completing-read "e/special: " completions))n
+         (output (cdr (assoc input special-inputs))))
+    (if output
+        (insert output)
+        (message "Not a shorthand: %s" input))))
+
+(provide 'eudoxia-special)
--- home/.emacs.d/eudoxia/eudoxia.el
+++ home/.emacs.d/eudoxia/eudoxia.el
@@ -6,36 +6,9 @@
 (require 'eudoxia-mode-line)
 (require 'eudoxia-olivetti)
 (require 'eudoxia-persian)
+(require 'eudoxia-special)
 (require 'eudoxia-splash)
 (require 'eudoxia-ui)
 (require 'eudoxia-xml)
-
-;;;;
-;;;; Custom Commands
-;;;;
-
-(defun e/persian ()
-  "Use the Persian input method."
-  (interactive)
-  (set-input-method "farsi-transliterate-banan"))
-
-(defvar special-inputs
-  '(;; Punctuation
-    ("--" . "–")
-    ("---" . "—")
-    ("..." . "…")
-    ;; Spanish
-    ("n~" . "ñ"))
-  "Map of shorthand to special characters.")
-
-(defun e/special ()
-  "Enter special characters."
-  (interactive)
-  (let* ((completions (mapcar #'car special-inputs))
-         (input (completing-read "e/special: " completions))n
-         (output (cdr (assoc input special-inputs))))
-    (if output
-        (insert output)
-        (message "Not a shorthand: %s" input))))
 
 (provide 'eudoxia)
