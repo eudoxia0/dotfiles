2a723324160427b992e0c71908cd5161caab025f
Fernando Borretti <fernando@borretti.me>
2024-06-15 11:03:55 +1000

    indent

--- darwin.nix
+++ darwin.nix
@@ -4,9 +4,9 @@
     services.nix-daemon.enable = true;
 
     nix.extraOptions = ''
-       auto-optimise-store = true
-       experimental-features = nix-command flakes
-       extra-platforms = x86_64-darwin aarch64-darwin
+        auto-optimise-store = true
+        experimental-features = nix-command flakes
+        extra-platforms = x86_64-darwin aarch64-darwin
     '';
 
     nixpkgs.hostPlatform = "aarch64-darwin";
