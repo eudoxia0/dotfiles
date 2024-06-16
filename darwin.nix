0ec2d2b13653061cd5f97caa92575e6234bfdb77
Fernando Borretti <fernando@borretti.me>
2024-06-16 12:37:50 +1000

    Fix indentation

--- darwin.nix
+++ darwin.nix
@@ -1,38 +1,38 @@
 { config, pkgs, ... }:
 
 {
-    services.nix-daemon.enable = true;
+  services.nix-daemon.enable = true;
 
-    nix.extraOptions = ''
+  nix.extraOptions = ''
         auto-optimise-store = true
         experimental-features = nix-command flakes
         extra-platforms = x86_64-darwin aarch64-darwin
     '';
 
-    nix.nixPath = [
-      "nixpkgs=${pkgs.path}"
+  nix.nixPath = [
+    "nixpkgs=${pkgs.path}"
+  ];
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  programs.zsh.enable = true;
+
+  users.users.eudoxia = {
+    home = "/Users/eudoxia";
+  };
+
+  system.defaults = {
+    dock = {
+      autohide = false;
+      orientation = "bottom";
+    };
+  };
+
+  homebrew = {
+    enable = true;
+
+    casks = [
+      "emacs"
     ];
-
-    nixpkgs.hostPlatform = "aarch64-darwin";
-
-    programs.zsh.enable = true;
-
-    users.users.eudoxia = {
-        home = "/Users/eudoxia";
-    };
-
-    system.defaults = {
-        dock = {
-            autohide = false;
-            orientation = "bottom";
-        };
-    };
-
-    homebrew = {
-        enable = true;
-
-        casks = [
-            "emacs"
-        ];
-    };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,59 +1,59 @@
 { pkgs, ... }:
 
 {
-    home = {
-        username = "eudoxia";
-        homeDirectory = "/Users/eudoxia";
-        stateVersion = "24.05";
+  home = {
+    username = "eudoxia";
+    homeDirectory = "/Users/eudoxia";
+    stateVersion = "24.05";
 
-        packages = with pkgs; [
-            coreutils
-            neofetch
-            curl
-            python3
-            rsync
-            cargo
-        ];
+    packages = with pkgs; [
+      coreutils
+      neofetch
+      curl
+      python3
+      rsync
+      cargo
+    ];
 
-        file = {
-            ".gitconfig".source = ./home/.gitconfig;
-            ".gitignore".source = ./home/.global-gitignore;
-            ".config/zed/settings.json".source = ./home/.config/zed/settings.json;
-            ".emacs.d/init.el".source = ./home/.emacs.d/init.el;
-            ".emacs.d/eudoxia/" = {
-                source = ./home/.emacs.d/eudoxia;
-                recursive = true;
-            };
-            ".hushlogin".text = "";
-            ".local/bin/backup.sh".source = ./home/.local/bin/backup.sh;
-            ".local/bin/canonicalize-screenshots.py".source = ./home/.local/bin/canonicalize-screenshots.py;
-            ".local/bin/docker-blow-away.sh".source = ./home/.local/bin/docker-blow-away.sh;
-            ".local/bin/timestamp.py".source = ./home/.local/bin/timestamp.py;
-        };
+    file = {
+      ".gitconfig".source = ./home/.gitconfig;
+      ".gitignore".source = ./home/.global-gitignore;
+      ".config/zed/settings.json".source = ./home/.config/zed/settings.json;
+      ".emacs.d/init.el".source = ./home/.emacs.d/init.el;
+      ".emacs.d/eudoxia/" = {
+        source = ./home/.emacs.d/eudoxia;
+        recursive = true;
+      };
+      ".hushlogin".text = "";
+      ".local/bin/backup.sh".source = ./home/.local/bin/backup.sh;
+      ".local/bin/canonicalize-screenshots.py".source = ./home/.local/bin/canonicalize-screenshots.py;
+      ".local/bin/docker-blow-away.sh".source = ./home/.local/bin/docker-blow-away.sh;
+      ".local/bin/timestamp.py".source = ./home/.local/bin/timestamp.py;
     };
+  };
 
-    programs = {
-        zsh = {
-            enable = true;
+  programs = {
+    zsh = {
+      enable = true;
 
-            shellAliases = {
-                ls   = "ls -1 --color";
-                gs   = "git status";
-                gits = "git status";
-                gb   = "git branch";
-                gco  = "git checkout";
-                gcam = "git commit -a -m";
-                gu   = "git push -u origin HEAD";
-                gd   = "git pull origin";
-                cf   = "cargo fmt";
-                ck   = "cargo check";
-            };
-            sessionVariables = {
-              PATH = "$HOME/.local/bin:$PATH";
-            };
-        };
-        home-manager = {
-            enable = true;
-        };
+      shellAliases = {
+        ls   = "ls -1 --color";
+        gs   = "git status";
+        gits = "git status";
+        gb   = "git branch";
+        gco  = "git checkout";
+        gcam = "git commit -a -m";
+        gu   = "git push -u origin HEAD";
+        gd   = "git pull origin";
+        cf   = "cargo fmt";
+        ck   = "cargo check";
+      };
+      sessionVariables = {
+        PATH = "$HOME/.local/bin:$PATH";
+      };
     };
+    home-manager = {
+      enable = true;
+    };
+  };
 }
