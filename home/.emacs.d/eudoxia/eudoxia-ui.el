5c22839a2b61f2e3b3d8b196c6824a4efe1d5686
Fernando Borretti <fernando@borretti.me>
2024-06-18 07:37:16 +1000

    fontsize++

--- home/.emacs.d/eudoxia/eudoxia-ui.el
+++ home/.emacs.d/eudoxia/eudoxia-ui.el
@@ -31,8 +31,8 @@
 (global-display-line-numbers-mode 1)
 
 ;; Default fonts.
-(set-face-attribute 'default nil :font "Inconsolata-15")
-(set-face-attribute 'mode-line nil :font "Inconsolata-15")
+(set-face-attribute 'default nil :font "Inconsolata-16")
+(set-face-attribute 'mode-line nil :font "Inconsolata-16")
 
 ;; Show trailing whitespace.
 (setq-default show-trailing-whitespace t)
