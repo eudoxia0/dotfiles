c473e76a883ca7d7dd2b705757e1b56ca61b968a
Fernando Borretti <fernando@borretti.me>
2024-06-22 16:25:32 +1000

    encrypt.sh

--- home/.local/bin/encrypt.sh
+++ home/.local/bin/encrypt.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
+if [ $# -ne 2 ]; then
+    echo "Error: This script requires exactly two arguments."
+    echo "Usage: encrypt.sh <input> <output>"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "Input file does not exist."
+    exit 1
+fi
+
+if [ -e "$2" ]; then
+    echo "Output file exists."
+    exit 1
+fi
+
 age -p $1 > $2
