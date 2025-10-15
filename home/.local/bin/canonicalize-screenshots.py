56f1fc2011989c70acc462972893c8c3d43e141b
Fernando Borretti <fernando@borretti.me>
2025-10-15 12:41:16 +1100

    format script

--- home/.local/bin/canonicalize-screenshots.py
+++ home/.local/bin/canonicalize-screenshots.py
@@ -4,12 +4,16 @@ Rewrites screenshot filenames from macOS format to
 
     YYYY-MM-DD-HHMMSS_macos.png
 """
+
 import os
 import re
 from datetime import datetime
 
 # Compile a regular expression pattern to match the original filename format
-pattern = re.compile(r'^Screenshot (\d{4}-\d{2}-\d{2}) at (\d{1,2}).(\d{2}).(\d{2})\s([apAP][mM])\.png$')
+pattern = re.compile(
+    r"^Screenshot (\d{4}-\d{2}-\d{2}) at (\d{1,2}).(\d{2}).(\d{2})\s([apAP][mM])\.png$"
+)
+
 
 def convert_filename(filename):
     match = pattern.match(filename)
@@ -22,10 +26,12 @@ def convert_filename(filename):
         return new_filename
     return None
 
+
 def rename_files():
-    for filename in os.listdir('.'):
+    for filename in os.listdir("."):
         new_filename = convert_filename(filename)
         if new_filename:
             os.rename(filename, new_filename)
+
 
 rename_files()
