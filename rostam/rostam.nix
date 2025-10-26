efd77f395fd8d562f0a967a817d13c3cc3e86855
Fernando Borretti <fernando@borretti.me>
2025-10-26 13:59:22 +1100

    fix locale

--- rostam/rostam.nix
+++ rostam/rostam.nix
@@ -18,7 +18,7 @@ in
   networking.hostName = "rostam";
   time.timeZone = "Australia/Sydney";
 
-  i18n.defaultLocale = "en_AU.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
