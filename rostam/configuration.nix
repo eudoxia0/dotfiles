7a4c997b078e62a52b547ecb2e49093796a631e2
Fernando Borretti <fernando@borretti.me>
2025-10-26 11:47:53 +1100

    format

--- rostam/configuration.nix
+++ rostam/configuration.nix
@@ -6,196 +6,196 @@ in
 {
   imports =
     [ ./hardware-configuration.nix
-    (import "${home-manager}/nixos")
+      (import "${home-manager}/nixos")
     ];
 
-    # Bootloader.
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    # Use latest kernel.
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-    boot.initrd.luks.devices."luks-5e2a0183-ed29-499d-8741-ea27e08caf28".device = "/dev/disk/by-uuid/5e2a0183-ed29-499d-8741-ea27e08caf28";
-    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  boot.initrd.luks.devices."luks-5e2a0183-ed29-499d-8741-ea27e08caf28".device = "/dev/disk/by-uuid/5e2a0183-ed29-499d-8741-ea27e08caf28";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-    # Configure network proxy if necessary
-    # networking.proxy.default = "http://user:password@proxy:port/";
-    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    # Enable networking
-    networking.networkmanager.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-    # Set your time zone.
-    time.timeZone = "Australia/Sydney";
+  # Set your time zone.
+  time.timeZone = "Australia/Sydney";
 
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_AU.UTF-8";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
 
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "en_AU.UTF-8";
-      LC_IDENTIFICATION = "en_AU.UTF-8";
-      LC_MEASUREMENT = "en_AU.UTF-8";
-      LC_MONETARY = "en_AU.UTF-8";
-      LC_NAME = "en_AU.UTF-8";
-      LC_NUMERIC = "en_AU.UTF-8";
-      LC_PAPER = "en_AU.UTF-8";
-      LC_TELEPHONE = "en_AU.UTF-8";
-      LC_TIME = "en_AU.UTF-8";
-    };
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
 
-    # Enable the X11 windowing system.
+  # Enable the X11 windowing system.
 
-    # Configure keymap in X11
-    services.xserver.xkb = {
-      layout = "us";
-      variant = "";
-    };
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
-    # Enable CUPS to print documents.
-    services.printing.enable = true;
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
-    # Enable sound with pipewire.
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
-    };
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.eudoxia = {
-      isNormalUser = true;
-      description = "eudoxia";
-      extraGroups = [ "networkmanager" "wheel" ];
-      packages = with pkgs; [
-        #  thunderbird
-      ];
-    };
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.eudoxia = {
+    isNormalUser = true;
+    description = "eudoxia";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      #  thunderbird
+    ];
+  };
 
-    # Install firefox.
-    programs.firefox.enable = true;
+  # Install firefox.
+  programs.firefox.enable = true;
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-      #   enable = true;
-      #   enableSSHSupport = true;
-      # };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
-      # List services that you want to enable:
+  # List services that you want to enable:
 
-      # Enable the OpenSSH daemon.
-      # services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-      # Open ports in the firewall.
-      # networking.firewall.allowedTCPPorts = [ ... ];
-      # networking.firewall.allowedUDPPorts = [ ... ];
-      # Or disable the firewall altogether.
-      # networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-      # This value determines the NixOS release from which the default
-      # settings for stateful data, like file locations and database versions
-      # on your system were taken. It‘s perfectly fine and recommended to leave
-      # this value at the release version of the first install of this system.
-      # Before changing this value read the documentation for this option
-      # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-      system.stateVersion = "25.05"; # Did you read the comment?
-
-
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
 
 
 
-      # CUSTOM
-      networking.hostName = "rostam";
-      nixpkgs.config.allowUnfree = true;
-      services.displayManager.ly.enable = true;
-      services.xserver.desktopManager.gnome.enable = true;
-      services.xserver.windowManager.stumpwm.enable = true;
-      services.xserver.windowManager.awesome.enable = true;
-      environment.systemPackages = with pkgs; [
-        grim # screenshot functionality
-        slurp # screenshot functionality
-        wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
-        mako # notification system developed by swaywm maintainer
-        curl
-        vim
-        foot
-        emacs
-        chromium
-        pcmanfm
-        xfce.thunar
-        rox-filer
-        mate.caja
-        spaceFM
-        nemo
-        xfe
-        cosmic-files
-        ghostty
-        lxrandr
-        git
-        arandr
-        kitty
-        alacritty
-        zed-editor
-        wdisplays
-        pciutils # lspci
-        usbutils # lsusb
-        todoist-electron
-      ];
-      programs.sway = {
-        enable = true;
-        wrapperFeatures.gtk = true;
-      };
-
-      programs._1password.enable = true;
-      programs._1password-gui = {
-        enable = true;
-        # Certain features, including CLI integration and system authentication support,
-        # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-        polkitPolicyOwners = [ "eudoxia" ];
-      };
-
-      environment.sessionVariables = {
-        GDK_SCALE = "";
-        GDK_DPI_SCALE = "";
-        NIXOS_OZONE_WL = "1";
-      };
 
 
-      services.xserver.enable = true;
-      # services.xserver.displayManager.sessionCommands = ''
-      #   xrandr --output HDMI-1 --mode 2560x1440
-      # '';
+  # CUSTOM
+  networking.hostName = "rostam";
+  nixpkgs.config.allowUnfree = true;
+  services.displayManager.ly.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.windowManager.stumpwm.enable = true;
+  services.xserver.windowManager.awesome.enable = true;
+  environment.systemPackages = with pkgs; [
+    grim # screenshot functionality
+    slurp # screenshot functionality
+    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+    mako # notification system developed by swaywm maintainer
+    curl
+    vim
+    foot
+    emacs
+    chromium
+    pcmanfm
+    xfce.thunar
+    rox-filer
+    mate.caja
+    spaceFM
+    nemo
+    xfe
+    cosmic-files
+    ghostty
+    lxrandr
+    git
+    arandr
+    kitty
+    alacritty
+    zed-editor
+    wdisplays
+    pciutils # lspci
+    usbutils # lsusb
+    todoist-electron
+  ];
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
 
-      programs.ssh = {
-        extraConfig = ''
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "eudoxia" ];
+  };
+
+  environment.sessionVariables = {
+    GDK_SCALE = "";
+    GDK_DPI_SCALE = "";
+    NIXOS_OZONE_WL = "1";
+  };
+
+
+  services.xserver.enable = true;
+  # services.xserver.displayManager.sessionCommands = ''
+  #   xrandr --output HDMI-1 --mode 2560x1440
+  # '';
+
+  programs.ssh = {
+    extraConfig = ''
           Host *
           IdentityAgent ~/.1password/agent.sock
         '';
-      };
+  };
 
-      # END CUSTOM
+  # END CUSTOM
 
-      # HOME MANAGER
-      home-manager.users.eve = { pkgs, ... }: {
-        home.packages = [ ];
-        programs.bash.enable = true;
+  # HOME MANAGER
+  home-manager.users.eve = { pkgs, ... }: {
+    home.packages = [ ];
+    programs.bash.enable = true;
 
-        # The state version is required and should stay at the version you
-        # originally installed.
-        home.stateVersion = "25.05";
-      };
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "25.05";
+  };
 }
