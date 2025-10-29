f49691a194d185d00b4e3c37ae999cc1a2d3cfa4
Fernando Borretti <fernando@borretti.me>
2025-10-29 18:55:39 +1100

    rostam: stumpwm: add a docstring for future me

--- hosts/rostam/config/stumpwmrc.lisp
+++ hosts/rostam/config/stumpwmrc.lisp
@@ -151,7 +151,9 @@
 ;;; Based on: https://github.com/stumpwm/stumpwm-contrib/tree/326725802fcedc2f8f28df91d4b548743da9e7bc/util/swm-gaps
 ;;;
 
-;; Head gaps run along the 4 borders of the monitor(s).
+;; Head gaps run along the 4 borders of the monitor(s). NOTE: if you
+;; have a systray (e.g. polybar) a positive value of this parameter
+;; will clip the sides of it.
 (defparameter *head-gaps-size* 0)
 
 ;; Inner gaps run along all the 4 borders of a window.
