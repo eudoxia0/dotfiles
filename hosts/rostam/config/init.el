ed64745d7c620bf5e19ce2e64985842e671d9d44
Fernando Borretti <fernando@borretti.me>
2025-11-01 13:57:30 +1100

    rostam/emacs: splash

--- hosts/rostam/config/init.el
+++ hosts/rostam/config/init.el
@@ -31,6 +31,28 @@
 ;; No splash screen.
 (setq inhibit-startup-screen t)
 
+;; Words of encouragement, from the SLIME source code.
+(defvar words-of-encouragement
+  (list "Let the hacking commence!"
+	"Hacks and glory await!"
+	"Hack and be merry!"
+	"Your hacking starts... NOW!"
+	"May the source be with you!"
+	"Take this REPL, brother, and may it serve you well."
+	"Lemonodor-fame is but a hack away!"
+	"Are we consing yet?"
+	(format "%s, this could be the start of a beautiful program." (user-login-name)))
+  "")
+
+(defun random-words-of-encouragement ()
+  "Return a string of hackerish encouragement. From the SLIME source code."
+  (nth (random (length words-of-encouragement))
+       words-of-encouragement))
+
+;; Display a custom message in the echo area.
+(defun display-startup-echo-area-message ()
+  (message (random-words-of-encouragement)))
+
 ;;;
 ;;; UI
 ;;;
@@ -155,11 +177,11 @@
   (interactive)
   (setq my-current-theme my-default-theme)
   (load-theme my-current-theme t)
-  (message "Theme: %s" my-current-theme))  
+  (message "Theme: %s" my-current-theme))
 
 (keymap-global-set "<f9>"    'theme-prev)
 (keymap-global-set "<f10>"   'theme-next)
-(keymap-global-set "C-<f10>" 'theme-reset)  
+(keymap-global-set "C-<f10>" 'theme-reset)
 
 ;;;
 ;;; etc.
