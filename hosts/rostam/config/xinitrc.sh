c1d2c11042716e3e0930786f36a5d5d15cd33c09
Fernando Borretti <fernando@borretti.me>
2025-11-01 13:40:33 +1100

    trim

--- hosts/rostam/config/xinitrc.sh
+++ hosts/rostam/config/xinitrc.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-# eval $(gnome-keyring-daemon --start --components=secrets,ssh)
-# export $(gnome-keyring-daemon --start --components=secrets,ssh)
-# dbus-update-activation-environment --systemd DISPLAY
-
 exec stumpwm
