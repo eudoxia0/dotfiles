1ccfddbf958e68e8546e5c5f3f8c36e8db439e46
Fernando Borretti <fernando@borretti.me>
2025-11-02 17:00:56 +1100

    ❄️ use flakes for rostam (#5)
    * add flake.nix
    
    * add flake.lock
    
    * update recrank/upgrade target
    
    * trim
    
    * remove unnnecessary command
    
    * only delete gens older than 7d
    
    * etc

---
+++ hosts/rostam/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "my nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      # home-manager should track the same nixpkgs as the system
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      rostam = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./rostam.nix
+
+          # Make home-manager use system pkgs
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+    };
+  };
+}
