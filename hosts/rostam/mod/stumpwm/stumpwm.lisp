c0429527d14ddd15932883f8bf13f4223e15572f
Fernando Borretti <fernando@borretti.me>
2025-11-02 21:14:42 +1100

    update paths

--- hosts/rostam/mod/stumpwm/stumpwm.lisp
+++ hosts/rostam/mod/stumpwm/stumpwm.lisp
@@ -146,11 +146,11 @@
 (defparameter *config-map*
   (let ((m (make-sparse-keymap)))
     ;; Open Emacs to the stumpwmrc configuration.
-    (define-key m (kbd "s") "exec emacs ~/dotfiles/hosts/rostam/config/stumpwmrc.lisp")
+    (define-key m (kbd "s") "exec emacs ~/dotfiles/hosts/rostam/mod/stumpwm/stumpwm.lisp")
     ;; Open Emacs to the Nix configuration for rostam.
     (define-key m (kbd "n") "exec emacs ~/dotfiles/hosts/rostam/rostam.nix")
     ;; Open Emacs to the Emacs init.el.
-    (define-key m (kbd "e") "exec emacs ~/dotfiles/hosts/rostam/config/init.el")
+    (define-key m (kbd "e") "exec emacs ~/dotfiles/hosts/rostam/mod/emacs/init.el")
     m))
 
 ;; Prefix key of the config map.
