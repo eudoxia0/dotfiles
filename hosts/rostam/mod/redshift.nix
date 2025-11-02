d7712d7c0cb30184dafbcab0fb80a93a5cf5a2bf
Fernando Borretti <fernando@borretti.me>
2025-11-03 07:11:58 +1100

    redshift: adjust temperature

--- hosts/rostam/mod/redshift.nix
+++ hosts/rostam/mod/redshift.nix
@@ -7,6 +7,10 @@
 
 {
   services.redshift.enable = true;
+  services.redshift.temperature = {
+    day   = 6500;
+    night = 3000;
+  };
   location = {
     latitude = -33.8;
     longitude = 151.2;
