9a1cb24b6afe51ce2c2ec9965deea7035c6d5cb9
Fernando Borretti <fernando@borretti.me>
2025-11-02 14:57:33 +1100

    rostam: set dpi to 168

--- hosts/rostam/rostam.nix
+++ hosts/rostam/rostam.nix
@@ -76,7 +76,7 @@ in
     layout = "us";
     variant = "";
   };
-  services.xserver.dpi = 192;
+  services.xserver.dpi = 168;
   services.xscreensaver.enable = true;
   services.redshift.enable = true;
   location = {
