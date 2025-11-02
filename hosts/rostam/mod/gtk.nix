9004a30807c1f100bfdd70f0380ce77dcb77cd68
Fernando Borretti <fernando@borretti.me>
2025-11-02 21:21:35 +1100

    set GDK_SCALE and GDK_DPI_SCALE

--- hosts/rostam/mod/gtk.nix
+++ hosts/rostam/mod/gtk.nix
@@ -1,6 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
+  environment.variables = {
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+  };
+
   home-manager.users.eudoxia.gtk = {
     enable = true;
     theme = {
