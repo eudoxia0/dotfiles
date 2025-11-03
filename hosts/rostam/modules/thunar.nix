20f71e5765e3f0a21dffdb8db488e96c68203154
Fernando Borretti <fernando@borretti.me>
2025-11-03 20:43:41 +1100

    .

--- hosts/rostam/modules/thunar.nix
+++ hosts/rostam/modules/thunar.nix
@@ -6,18 +6,26 @@
 }:
 
 {
+  programs.thunar = {
+    enable = true;
+    plugins = [
+      pkgs.xfce.thunar-archive-plugin
+      pkgs.xfce.thunar-volman
+    ];
+  }
+
+  # mount etc.
+  services.gvfs.enable = true;
+
+  # thumbnails service
+  services.tumbler.enable = true;
+
   home-manager.users.eudoxia.home.packages = with pkgs; [
-    xfce.thunar
     ffmpegthumbnailer # video thumbnails
     libgsf # odf thumbnails
     poppler # pdf thumbnails
     libraw # RAW thumbnails
     webp-pixbuf-loader # webp thumbnail
-    xfce.tumbler # xfce thumbnail service
     xfce.xfconf # query xfce config
   ];
-
-  home-manager.users.eudoxia.xfconf.settings = {
-
-  };
 }
