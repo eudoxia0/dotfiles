e69d215eb03f05f155264404d81b26932eddc589
Fernando Borretti <fernando@borretti.me>
2025-11-03 13:37:18 +1100

    firefox: install 1password extension

--- hosts/rostam/mod/firefox/default.nix
+++ hosts/rostam/mod/firefox/default.nix
@@ -58,6 +58,10 @@
           "installation_mode" = "normal_installed";
           "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
         };
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          "installation_mode" = "normal_installed";
+          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+        };
       };
     };
 
