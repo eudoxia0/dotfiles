233caee302fc7bec30fb9f5c80f7d5e902da8f64
Fernando Borretti <fernando@borretti.me>
2025-11-03 14:11:58 +1100

    fmt

--- hosts/rostam/mod/firefox/default.nix
+++ hosts/rostam/mod/firefox/default.nix
@@ -57,12 +57,14 @@
         # uBlock Origin
         "uBlock0@raymondhill.net" = {
           "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
+          "install_url" =
+            "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
         };
         # 1Password
         "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
           "installation_mode" = "normal_installed";
-          "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+          "install_url" =
+            "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
         };
       };
     };
