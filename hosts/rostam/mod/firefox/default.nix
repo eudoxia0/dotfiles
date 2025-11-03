1729becec2924b8ac255d6f9484b7c15adba5a70
Fernando Borretti <fernando@borretti.me>
2025-11-03 13:37:54 +1100

    comment

--- hosts/rostam/mod/firefox/default.nix
+++ hosts/rostam/mod/firefox/default.nix
@@ -54,10 +54,12 @@
 
       # Install extensions.
       ExtensionSettings = {
+        # uBlock Origin
         "uBlock0@raymondhill.net" = {
           "installation_mode" = "normal_installed";
           "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
         };
+        # 1Password
         "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
           "installation_mode" = "normal_installed";
           "install_url" = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
