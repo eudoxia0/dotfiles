cb4e97bc7107b9a5917cde332eb2f26519e7bf6c
Fernando Borretti <fernando@borretti.me>
2025-11-03 15:40:00 +1100

    change separator

--- hosts/rostam/modules/polybar/world-clock.sh
+++ hosts/rostam/modules/polybar/world-clock.sh
@@ -9,4 +9,4 @@ NYC=$(TZ="America/New_York" date +"%H:%M")
 LDN=$(TZ="Europe/London" date +"%H:%M")
 
 # Use polybar color codes: %{F#707880} for muted text, %{F-} to reset to default
-echo "%{F#707880}MNL%{F-} $MNL | %{F#707880}SF%{F-} $SF | %{F#707880}NYC%{F-} $NYC | %{F#707880}LDN%{F-} $LDN"
+echo "%{F#707880}MNL%{F-} $MNL - %{F#707880}SF%{F-} $SF - %{F#707880}NYC%{F-} $NYC - %{F#707880}LDN%{F-} $LDN"
