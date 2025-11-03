615b04856a918c66c4304679f61bfcbd271cacf0
Fernando Borretti <fernando@borretti.me>
2025-11-04 00:36:09 +1100

    sort

--- hosts/rostam/modules/cli.nix
+++ hosts/rostam/modules/cli.nix
@@ -8,13 +8,13 @@
 {
   home-manager.users.eudoxia.home.packages = with pkgs; [
     age
+    djvu2pdf
     fastfetch
+    file
     neofetch
     pciutils # lspci
     taskwarrior-tui
     taskwarrior3
     usbutils # lsusb
-    file
-    djvu2pdf
   ];
 }
