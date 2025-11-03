54f080bffffedbddc016a6de3ae97af634bb9194
Fernando Borretti <fernando@borretti.me>
2025-11-03 18:10:03 +1100

    change the gtk theme

--- hosts/rostam/modules/gtk.nix
+++ hosts/rostam/modules/gtk.nix
@@ -9,12 +9,19 @@
   home-manager.users.eudoxia.gtk = {
     enable = true;
     theme = {
-      name = "Arc";
-      package = pkgs.arc-theme;
+      name = "Adwaita";
     };
     iconTheme = {
-      name = "WhiteSur";
-      package = pkgs.whitesur-icon-theme;
+      name = "elementary";
+      package = pkgs.pantheon.elementary-icon-theme;
     };
   };
+
+  # Install other themes so we can preview them.
+  home-manager.users.eudoxia.home.packages = with pkgs; [
+    pantheon.elementary-gtk-theme
+    pantheon.elementary-icon-theme
+    adwaita-icon-theme-legacy
+    arc-theme
+  ];
 }
