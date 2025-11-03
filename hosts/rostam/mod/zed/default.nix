e242991c64c50bb07669bf201955fdabbaef62fe
Fernando Borretti <fernando@borretti.me>
2025-11-03 11:38:58 +1100

    add zed settings.json

--- hosts/rostam/mod/zed/default.nix
+++ hosts/rostam/mod/zed/default.nix
@@ -9,4 +9,7 @@
   home-manager.users.eudoxia.home.packages = with pkgs; [
     zed-editor
   ];
+  home-manager.users.eudoxia.home.file = {
+    ".config/zed/settings.json".source = ./settings.json;
+  };
 }
