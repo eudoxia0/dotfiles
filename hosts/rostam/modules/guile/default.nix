4a7ccc9186d96476e4a185aa35893473a49efeaf
Fernando Borretti <fernando@borretti.me>
2025-11-03 17:13:32 +1100

    add guile modules

--- hosts/rostam/modules/guile/default.nix
+++ hosts/rostam/modules/guile/default.nix
@@ -6,10 +6,17 @@
 }:
 
 {
-
   home-manager.users.eudoxia.home.packages = with pkgs; [
     guile
   ];
 
   home-manager.users.eudoxia.home.file.".guile".source = ./init.scm;
+  home-manager.users.eudoxia.home.file.".guile-modules/eudoxia" = {
+    source = ./eudoxia;
+    recursive = true;
+  };
+
+  home-manager.users.eudoxia.home.sessionVariables = {
+    GUILE_LOAD_PATH = "/home/eudoxia/.guile-modules";
+  };
 }
---
+++ hosts/rostam/modules/guile/eudoxia/hello.scm
@@ -0,0 +1,5 @@
+(define-module (eudoxia hello)
+  #:export (hello-world))
+
+(define (hello-world)
+  (display "Hello, world!\n"))
