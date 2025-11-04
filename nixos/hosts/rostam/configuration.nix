9778989112a3a84359e7a3aa7ccc0e5a229fd023
Fernando Borretti <fernando@borretti.me>
2025-11-05 08:38:28 +1100

    rostam: remove antigone from syncthing

--- nixos/hosts/rostam/configuration.nix
+++ nixos/hosts/rostam/configuration.nix
@@ -77,17 +77,13 @@ in
         name = "rostam";
         id = "H6XRBBB-7WXJPGN-GVMFX3T-BANURMZ-B5CYCIZ-ZMVNFEB-HQWGIXE-3AZ5EQ4";
       };
-      antigone = {
-        name = "antigone";
-        id = "SZYNWMU-QR6FOET-AZ6UYIV-S4J7YZN-KQIEA5Q-N3BRPBT-VQGIAOR-3MSZTA7";
-      };
     };
     settings.folders = {
       "root" = {
         id = "root";
         label = "Root";
         path = "/home/eudoxia/Root";
-        devices = [ "antigone" ];
+        devices = [];
       };
     };
   };
