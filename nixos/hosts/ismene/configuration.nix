01a8741928c672831abd321f593de9cfacf40102
Fernando Borretti <fernando@borretti.me>
2025-11-04 18:45:23 +1100

    ismene: install 1password

--- nixos/hosts/ismene/configuration.nix
+++ nixos/hosts/ismene/configuration.nix
@@ -133,4 +133,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
+  # programs
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+  };
 }
