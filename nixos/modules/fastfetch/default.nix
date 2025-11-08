8cb2ef3a2a4505c451ded01e66065902b4f3475a
Fernando Borretti <fernando@borretti.me>
2025-11-08 11:21:05 +1100

    fmt

--- nixos/modules/fastfetch/default.nix
+++ nixos/modules/fastfetch/default.nix
@@ -10,6 +10,7 @@
     fastfetch
   ];
 
-  home-manager.users.eudoxia.home.file.".local/share/eudoxia/nerv-color.txt".source = ./nerv/nerv-color.txt;
+  home-manager.users.eudoxia.home.file.".local/share/eudoxia/nerv-color.txt".source =
+    ./nerv/nerv-color.txt;
   home-manager.users.eudoxia.home.file.".local/bin/nervfetch".source = ./nerv/nervfetch.sh;
 }
