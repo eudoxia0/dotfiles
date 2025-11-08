dd372b98aec669004e2c0448e4a0930cc4e62c18
Fernando Borretti <fernando@borretti.me>
2025-11-08 12:11:53 +1100

    ly: change animation from doom to colormix

--- nixos/modules/ly/default.nix
+++ nixos/modules/ly/default.nix
@@ -8,15 +8,13 @@
 {
   services.displayManager.ly.enable = true;
   services.displayManager.ly.settings = {
-    animation = "doom";
+    animation = "colormix";
     bigclock = "en";
     brightness_down_key = "null";
     brightness_up_key = "null";
     clear_password = true;
     custom_sessions = "/etc/xdg/wayland-sessions/";
     default_input = "password";
-    doom_fire_height = 8;
-    doom_fire_spread = 4;
     hide_version_string = true;
   };
   home-manager.users.eudoxia.home.file = {
