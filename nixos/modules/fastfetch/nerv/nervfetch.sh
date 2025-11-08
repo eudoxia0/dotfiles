0c5bc442a979b348df7d093afa58eb3362e6d09f
Fernando Borretti <fernando@borretti.me>
2025-11-08 11:23:31 +1100

    add appropriate colours to output

--- nixos/modules/fastfetch/nerv/nervfetch.sh
+++ nixos/modules/fastfetch/nerv/nervfetch.sh
@@ -7,4 +7,7 @@ fastfetch --logo ~/.local/share/eudoxia/nerv-color.txt \
           --logo-color-4 '#990000' \
           --logo-color-5 '#770000' \
           --logo-color-6 '#550000' \
-          --logo-color-7 '#330000'
+          --logo-color-7 '#330000' \
+          --color-keys   '#CC0000' \
+          --color-title  '#FF0000' \
+          --color-output '#888888'
