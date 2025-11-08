6479a439ccea3bcb4bf9f9656f0a88b0064285db
Fernando Borretti <fernando@borretti.me>
2025-11-08 11:21:02 +1100

    copy nervfetch.sh

--- nixos/modules/fastfetch/default.nix
+++ nixos/modules/fastfetch/default.nix
@@ -11,4 +11,5 @@
   ];
 
   home-manager.users.eudoxia.home.file.".local/share/eudoxia/nerv-color.txt".source = ./nerv/nerv-color.txt;
+  home-manager.users.eudoxia.home.file.".local/bin/nervfetch".source = ./nerv/nervfetch.sh;
 }
