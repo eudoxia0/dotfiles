bb194f3036a6f10549e3937b6885603ca91b02c0
Fernando Borretti <fernando@borretti.me>
2025-11-08 11:20:59 +1100

    add nervfetch.sh

---
+++ nixos/modules/fastfetch/nerv/nervfetch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+fastfetch --logo ~/.local/share/eudoxia/nerv-color.txt \
+          --logo-color-1 '#FF0000' \
+          --logo-color-2 '#DD0000' \
+          --logo-color-3 '#BB0000' \
+          --logo-color-4 '#990000' \
+          --logo-color-5 '#770000' \
+          --logo-color-6 '#550000' \
+          --logo-color-7 '#330000'
