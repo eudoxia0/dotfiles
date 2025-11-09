f45cb8ecc1f6353ea55611b095f6962b9e0ed502
Fernando Borretti <fernando@borretti.me>
2025-11-09 14:15:32 +1100

    move system packages to home packages

--- nixos/modules/dev.nix
+++ nixos/modules/dev.nix
@@ -6,23 +6,20 @@
 }:
 
 {
-  environment.systemPackages = with pkgs; [
-    clang
-    curl
-    gnumake
-    sqlite
-    vim
-  ];
-
   home-manager.users.eudoxia.home.packages = with pkgs; [
     agda
     btop
+    clang
+    curl
     fd
+    gnumake
     imagemagick
     inform7
     just
     nixfmt-tree
+    sqlite
     tokei
     tree
+    vim
   ];
 }
