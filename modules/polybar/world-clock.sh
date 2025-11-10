424e148210c05acd9f330b4696c59b0494bb6115
Fernando Borretti <fernando@borretti.me>
2025-11-10 22:00:26 +1100

    fix

--- modules/polybar/world-clock.sh
+++ modules/polybar/world-clock.sh
@@ -2,10 +2,13 @@
 
 # World clock script for polybar
 
-SF=$(TZ="America/Los_Angeles" date +"%H:%M")
-NYC=$(TZ="America/New_York" date +"%H:%M")
-LDN=$(TZ="Europe/London" date +"%H:%M")
-MNL=$(TZ="Asia/Manila" date +"%H:%M")
+SF  = $(TZ="America/Los_Angeles" date +"%H:%M")
+NYC = $(TZ="America/New_York" date +"%H:%M")
+LDN = $(TZ="Europe/London" date +"%H:%M")
+MNL = $(TZ="Asia/Manila" date +"%H:%M")
 
 # Use polybar color codes: %{F#707880} for muted text, %{F-} to reset to default
-echo "%{F#707880}SF%{F-} $SF %{F#707880} NYC%{F-} $NYC %{F#707880} LDN%{F-} $LDN %{F#707880} MNL%{F-} $MNL"
+echo -n "%{F#707880}SF%{F-} $SF  "
+echo -n "%{F#707880}NYC%{F-} $NYC  "
+echo -n "%{F#707880}LDN%{F-} $LDN  "
+echo    "%{F#707880}MNL%{F-} $MNL"
