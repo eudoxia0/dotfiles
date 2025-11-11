721063dc8c1fb20e4704566a59e6835e1ca3668e
Fernando Borretti <fernando@borretti.me>
2025-11-11 14:46:24 +1100

    emacs: configure projectile

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -210,12 +210,23 @@
 (setq markdown-mouse-follow-link nil)
 
 ;;;
-;;; Vertico
+;;; Mode: Vertico
 ;;;
 
 (use-package vertico
   :init
   (vertico-mode))
+
+;;;
+;;; Mode: Projectile
+;;;
+
+(use-package projectile
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
 
 ;;;
 ;;; Mode: Inform7
