c408b4d792eba06779fc8d769db910aef2c0e103
Fernando Borretti <fernando@borretti.me>
2025-11-13 10:16:01 +1100

    sigh

--- modules/stumpwm/stumpwm.lisp
+++ modules/stumpwm/stumpwm.lisp
@@ -139,7 +139,6 @@
 ;;;;
 ;;;; Based on: https://github.com/stumpwm/stumpwm-contrib/tree/326725802fcedc2f8f28df91d4b548743da9e7bc/util/swm-gaps
 ;;;;
-(in-package :stumpwm)
 
 ;; Head gaps run along the 4 borders of the monitor(s). NOTE: if you
 ;; have a systray (e.g. polybar) a positive value of this parameter
