6a01a0dca9382f3c2256dd7a6a682642e801da6f
Fernando Borretti <fernando@borretti.me>
2025-11-13 10:07:57 +1100

    fmt

--- modules/wallpaper/default.nix
+++ modules/wallpaper/default.nix
@@ -7,5 +7,6 @@
 
 {
   home-manager.users.eudoxia.home.file.".eudoxia.d/data/wallpaper/panther.jpg".source = ./panther.jpg;
-  home-manager.users.eudoxia.home.file.".eudoxia.d/data/wallpaper/semiramis.jpg".source = ./semiramis.jpg;
+  home-manager.users.eudoxia.home.file.".eudoxia.d/data/wallpaper/semiramis.jpg".source =
+    ./semiramis.jpg;
 }
