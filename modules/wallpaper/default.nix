7545400f29cd89b1a5179809ece747dcad4b652e
Fernando Borretti <fernando@borretti.me>
2025-11-13 09:36:01 +1100

    copy wallpaper

--- modules/wallpaper/default.nix
+++ modules/wallpaper/default.nix
@@ -7,4 +7,5 @@
 
 {
   home-manager.users.eudoxia.home.file.".eudoxia.d/data/wallpaper/panther.jpg".source = ./panther.jpg;
+  home-manager.users.eudoxia.home.file.".eudoxia.d/data/wallpaper/semiramis.jpg".source = ./semiramis.jpg;
 }
