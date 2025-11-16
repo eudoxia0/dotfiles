f8b8c0815110d27af167b81316ee2e0a5c3cfbff
Fernando Borretti <fernando@borretti.me>
2025-11-16 21:00:00 +1100

    redshift: add scripts to start, stop the systemd service

--- modules/redshift.nix
+++ modules/redshift.nix
@@ -15,4 +15,21 @@
     latitude = -33.8;
     longitude = 151.2;
   };
+
+  home-manager.users.eudoxia.home.file = {
+    ".eudoxia.d/bin/start-redshift" = {
+      text = ''
+        #/bin/sh
+        systemctl --user start redshift
+      '';
+      executable = true;
+    };
+    ".eudoxia.d/bin/stop-redshift" = {
+      text = ''
+        #/bin/sh
+        systemctl --user stop redshift
+      '';
+      executable = true;
+    };
+  };
 }
