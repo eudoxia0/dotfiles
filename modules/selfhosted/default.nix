2b21784a48300fa2d6da2f61c0befaca6e559ba9
Fernando Borretti <fernando@borretti.me>
2025-11-18 12:05:30 +1100

    package koboexport

--- modules/selfhosted/default.nix
+++ modules/selfhosted/default.nix
@@ -5,11 +5,29 @@
   ...
 }:
 
+let
+  customPackages = {
+    koboexport = pkgs.rustPlatform.buildRustPackage {
+      pname = "koboexport";
+      version = "unstable";
+      src = pkgs.fetchgit {
+        url = "https://github.com/eudoxia0/koboexport.git";
+        rev = "HEAD";
+        sha256 = "sha256-Tt0fpqU3Rj0dtgr+XQwagm6K1auOvZ5hTFdJREno8/Y=";
+      };
+      cargoHash = "sha256-EI2h0jgNVtcp2ftxToHnEOHYJ3SoLhcLZkop+iBjTQY=";
+    };
+  };
+in
 {
   home-manager.users.eudoxia = {
     home.file = {
       ".config/zetanom/config.toml".source = ./zetanom.toml;
     };
+
+    home.packages = [
+      customPackages.koboexport
+    ];
 
     systemd.user.services.zetanom = {
       Unit = {
