3ee2afd4e8308102fe93e1e9e8dbedb9d332e7d6
Fernando Borretti <fernando@borretti.me>
2025-11-18 12:53:48 +1100

    fix hashcards package

--- modules/selfhosted/default.nix
+++ modules/selfhosted/default.nix
@@ -6,6 +6,11 @@
 }:
 
 let
+  katexTarball = pkgs.fetchurl {
+    url = "https://github.com/KaTeX/KaTeX/releases/download/v0.16.25/katex.tar.gz";
+    sha256 = "sha256-RUPIiaGi3s+iZ3NFjfZybEGRx+wwplre4l0Woojm1SU=";
+  };
+
   customPackages = {
     koboexport = pkgs.rustPlatform.buildRustPackage {
       pname = "koboexport";
@@ -18,16 +23,26 @@ let
       cargoHash = "sha256-EI2h0jgNVtcp2ftxToHnEOHYJ3SoLhcLZkop+iBjTQY=";
     };
 
-    # hashcards = pkgs.rustPlatform.buildRustPackage {
-    #   pname = "hashcards";
-    #   version = "unstable";
-    #   src = pkgs.fetchgit {
-    #     url = "https://github.com/eudoxia0/hashcards.git";
-    #     rev = "HEAD";
-    #     sha256 = "sha256-WaduFfG05aZFjKogO8Q65Doj7RkPtILoYhJKBdNiLOM=";
-    #   };
-    #   cargoHash = "sha256-c2RWugFHzgiVmVuwGlgOILJo/Y1QysLs9jU4GCWgxjc=";
-    # };
+    hashcards = pkgs.rustPlatform.buildRustPackage {
+      pname = "hashcards";
+      version = "unstable";
+      src = pkgs.fetchgit {
+        url = "https://github.com/eudoxia0/hashcards.git";
+        rev = "HEAD";
+        sha256 = "sha256-WaduFfG05aZFjKogO8Q65Doj7RkPtILoYhJKBdNiLOM=";
+      };
+      cargoHash = "sha256-c2RWugFHzgiVmVuwGlgOILJo/Y1QysLs9jU4GCWgxjc=";
+
+      preBuild = ''
+        mkdir -p vendor
+        tar -xzf ${katexTarball} -C vendor
+        # Rewrite font paths in CSS
+        substituteInPlace vendor/katex/katex.min.css \
+          --replace 'fonts/' '/katex/fonts/'
+      '';
+
+      nativeBuildInputs = [ pkgs.gnutar pkgs.gzip ];
+    };
   };
 in
 {
@@ -38,7 +53,7 @@ in
 
     home.packages = [
       customPackages.koboexport
-      # customPackages.hashcards
+      customPackages.hashcards
     ];
 
     systemd.user.services.zetanom = {
