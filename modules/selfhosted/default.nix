d89db07628010e6f16ed6ee3d719268122983424
Fernando Borretti <fernando@borretti.me>
2025-11-19 19:17:10 +1100

    use a specific koboexport commit

--- modules/selfhosted/default.nix
+++ modules/selfhosted/default.nix
@@ -17,7 +17,7 @@ let
       version = "unstable";
       src = pkgs.fetchgit {
         url = "https://github.com/eudoxia0/koboexport.git";
-        rev = "HEAD";
+        rev = "5500ee5c08577c8eebded61389addabdcd900a56";
         sha256 = "sha256-Tt0fpqU3Rj0dtgr+XQwagm6K1auOvZ5hTFdJREno8/Y=";
       };
       cargoHash = "sha256-EI2h0jgNVtcp2ftxToHnEOHYJ3SoLhcLZkop+iBjTQY=";
