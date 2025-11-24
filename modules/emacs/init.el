e2a672a89c214cc27f2368b330d25b5042248e50
Fernando Borretti <fernando@borretti.me>
2025-11-24 15:12:38 +1100

    .

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -388,11 +388,12 @@
 ;;; Mode: web
 ;;;
 
-;; Open `.jinja2` files in web-mode.
-(add-to-list 'auto-mode-alist '("\\.jinja2\\'" . web-mode))
-
-;; Use the `django` template engine for Jinja2 files.
-(add-to-list 'web-mode-engines-alist '("django" . "\\.jinja2\\'"))
+(use-package web-mode
+  :init
+  ;; Open `.jinja2` files in web-mode.
+  (add-to-list 'auto-mode-alist '("\\.jinja2\\'" . web-mode))
+  ;; Use the `django` template engine for Jinja2 files.
+  (add-to-list 'web-mode-engines-alist '("django" . "\\.jinja2\\'")))
 
 ;;;
 ;;; Quick Commit
