97f6ff2e716a67eb74a8578729b9c3e57210ef20
Fernando Borretti <fernando@borretti.me>
2025-11-26 13:02:24 +1100

    give up

--- modules/ly/default.nix
+++ modules/ly/default.nix
@@ -13,17 +13,9 @@
     brightness_down_key = "null";
     brightness_up_key = "null";
     clear_password = true;
-    custom_sessions = "/etc/xdg/custom-sessions/";
     default_input = "password";
     hide_version_string = true;
   };
-
-  environment.etc."xdg/custom-sessions/tty.desktop".text = ''
-    [Desktop Entry]
-    Name=tty
-    Exec=${pkgs.util-linux}/bin/setsid ${pkgs.nushell}/bin/nu --login
-    Type=Application
-  '';
 
   home-manager.users.eudoxia.home.file = {
     ".eudoxia.d/bin/restart-ly".source = ./restart-ly.sh;
