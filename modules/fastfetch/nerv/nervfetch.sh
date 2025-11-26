81c5a2015a484a2db53d931ccd7c75a3bd250013
Fernando Borretti <fernando@borretti.me>
2025-11-26 18:18:34 +1100

    nervfetch.sh: better colours

--- modules/fastfetch/nerv/nervfetch.sh
+++ modules/fastfetch/nerv/nervfetch.sh
@@ -5,9 +5,9 @@ fastfetch --logo ~/.eudoxia.d/data/nerv/nerv-color.txt \
           --logo-color-2 '#DD0000' \
           --logo-color-3 '#BB0000' \
           --logo-color-4 '#990000' \
-          --logo-color-5 '#770000' \
-          --logo-color-6 '#550000' \
-          --logo-color-7 '#330000' \
-          --color-keys   '#CC0000' \
+          --logo-color-5 '#880000' \
+          --logo-color-6 '#770000' \
+          --logo-color-7 '#660000' \
+          --color-keys   '#550000' \
           --color-title  '#FF0000' \
           --color-output '#888888'
