cdfe41321b436d33cf7f3c8a86774ba29e8b069f
Fernando Borretti <fernando@borretti.me>
2025-11-26 12:42:38 +1100

    add custom ly session for the tty

--- modules/ly/default.nix
+++ modules/ly/default.nix
@@ -13,10 +13,19 @@
     brightness_down_key = "null";
     brightness_up_key = "null";
     clear_password = true;
-    custom_sessions = "/etc/xdg/wayland-sessions/";
+    custom_sessions = "/etc/xdg/custom-sessions/";
     default_input = "password";
     hide_version_string = true;
   };
+
+  environment.etc."xdg/custom-sessions/tty.desktop".text = ''
+    [Desktop Entry]
+    Name=tty
+    Exec=${pkgs.nushell}/bin/nu
+    DesktopNames=null
+    Terminal=true
+  '';
+
   home-manager.users.eudoxia.home.file = {
     ".eudoxia.d/bin/restart-ly".source = ./restart-ly.sh;
   };
