65d6443dedee17d2e57f58e4c53857f8e7cbb3b8
Fernando Borretti <fernando@borretti.me>
2025-12-20 12:44:48 +1100

    fix syntax

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -6,22 +6,23 @@
 }:
 
 let
-  headphones = "88:0E:85:B1:FD:8E"
-  earbuds = "D8:19:7A:04:C8:E3"
+  headphones = "88:0E:85:B1:FD:8E";
+  earbuds = "D8:19:7A:04:C8:E3";
 
   connect-headphones = pkgs.writeShellScriptBin "connect-headphones" ''
     bluetoothctl connect ${headphones}
-  ''
+  '';
   disconnect-headphones = pkgs.writeShellScriptBin "disconnect-headphones" ''
     bluetoothctl disconnect ${headphones}
-  ''
+  '';
 
   connect-earbuds = pkgs.writeShellScriptBin "connect-earbuds" ''
     bluetoothctl connect ${earbuds}
-  ''
+  '';
   disconnect-earbuds = pkgs.writeShellScriptBin "disconnect-earbuds" ''
     bluetoothctl disconnect ${earbuds}
-  ''
+  '';
+in
 {
   hardware.bluetooth.enable = true;
 
