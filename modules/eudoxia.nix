01212ae9dfbaf3266cc2930830d94a3f5e2f8e01
Fernando Borretti <fernando@borretti.me>
2025-12-27 09:52:02 +1100

    periodically delete xdg dirs

--- modules/eudoxia.nix
+++ modules/eudoxia.nix
@@ -20,4 +20,28 @@
     enable = true;
     createDirectories = false;
   };
+
+  # Periodically remove unwanted XDG directories that applications recreate
+  home-manager.users.eudoxia.systemd.user.services.cleanup-xdg-dirs = {
+    Unit = {
+      Description = "Clean up unwanted XDG user directories";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'cd ~ && rmdir Desktop Documents Downloads 2>/dev/null || true'";
+    };
+  };
+
+  home-manager.users.eudoxia.systemd.user.timers.cleanup-xdg-dirs = {
+    Unit = {
+      Description = "Timer for cleaning up unwanted XDG user directories";
+    };
+    Timer = {
+      OnBootSec = "5m";
+      OnUnitActiveSec = "1h";
+    };
+    Install = {
+      WantedBy = [ "timers.target" ];
+    };
+  };
 }
