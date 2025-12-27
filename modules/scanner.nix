8404eefcaf04763a8308547a436c81622a370c08
Fernando Borretti <fernando@borretti.me>
2025-12-27 11:08:35 +1100

    enable brscan5

--- modules/scanner.nix
+++ modules/scanner.nix
@@ -13,7 +13,7 @@
   users.users.eudoxia.extraGroups = [ "scanner" "lp" ];
 
   # Add support for Brother scanners.
-  hardware.sane.brscan4.enable = true;
+  hardware.sane.brscan5.enable = true;
 
   # Install GNOME Document Scanner.
   home-manager.users.eudoxia.home.packages = with pkgs; [
