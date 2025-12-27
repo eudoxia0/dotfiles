b07373311caa48a8e733b8bad7831f3fa2274231
Fernando Borretti <fernando@borretti.me>
2025-12-28 07:23:19 +1100

    delete add-flashcard.py script

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -8,7 +8,6 @@
 {
   home-manager.users.eudoxia.home.file = {
     ".eudoxia.d/bin/add-to-dictionary".source = ./add-to-dictionary.py;
-    ".eudoxia.d/bin/add-flashcard".source = ./add-flashcard.py;
     ".eudoxia.d/bin/add-feed".source = ./add-feed.py;
   };
 }
