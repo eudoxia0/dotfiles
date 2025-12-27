9198d6c108b138a890bb612d46b59fefdc715ca5
Fernando Borretti <fernando@borretti.me>
2025-12-27 13:19:04 +1100

    firefox: remove 1password extension

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -72,12 +72,6 @@
           "install_url" =
             "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
         };
-        # 1Password
-        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-          "installation_mode" = "normal_installed";
-          "install_url" =
-            "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
-        };
         # Double-Click Image Downloader: https://github.com/eudoxia0/double-click-image-downloader
         "{5be538ce-6708-4b66-b3ce-81a25d2cbb0d}" = {
           "installation_mode" = "normal_installed";
