f074df6c5150ef382846d2bb23c15911f419faf2
Fernando Borretti <fernando@borretti.me>
2025-12-31 12:22:39 +1100

    less output

--- modules/eudoxia.nix
+++ modules/eudoxia.nix
@@ -8,28 +8,15 @@
 let
   cleanupXdgScript = pkgs.writeShellScript "cleanup-xdg-dirs" ''
     # Remove unwanted XDG user directories that applications recreate
-    echo "Starting cleanup, HOME=$HOME, PWD=$PWD"
-
     cd "$HOME" || {
-      echo "ERROR: Failed to cd to HOME"
       exit 1
     }
 
     for dir in Desktop Documents Downloads; do
       if [ -d "$dir" ]; then
-        echo "Attempting to remove $dir"
-        error_output=$(${pkgs.coreutils}/bin/rmdir "$dir" 2>&1)
-        if [ $? -eq 0 ]; then
-          echo "Successfully removed $dir"
-        else
-          echo "Failed to remove $dir: $error_output"
-        fi
-      else
-        echo "$dir does not exist"
+        ${pkgs.coreutils}/bin/rmdir "$dir"
       fi
     done
-
-    echo "Cleanup complete"
   '';
 in
 {
