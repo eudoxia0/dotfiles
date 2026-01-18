3e563eaaf86393c67ae93b5229b63855c5ad9804
Fernando Borretti <fernando@borretti.me>
2026-01-18 13:56:20 +1100

    remove config-map

--- modules/stumpwm/init.lisp
+++ modules/stumpwm/init.lisp
@@ -105,19 +105,6 @@
 (defprefixkey "x" "exec xscreensaver-command --lock")
 (defkey "Pause" "exec xscreensaver-command --lock")
 
-(defparameter *config-map*
-  (let ((m (make-sparse-keymap)))
-    ;; Open Emacs to the stumpwmrc configuration.
-    (define-key m (kbd "s") "exec emacs ~/dotfiles/modules/stumpwm/init.lisp")
-    ;; Open Emacs to the dotfiles flake.
-    (define-key m (kbd "n") "exec emacs ~/dotfiles/flake.nix")
-    ;; Open Emacs to the Emacs init.el.
-    (define-key m (kbd "e") "exec emacs ~/dotfiles/modules/emacs/init.el")
-    m))
-
-;; Prefix key of the config map.
-(define-key *root-map* (kbd "c") '*config-map*)
-
 ;; Toggle gaps on/off.
 (defprefixkey "g" "toggle-gaps")
 
