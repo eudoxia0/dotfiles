efe1b581eb789f7c63df356d9cd4c897deb323ef
Fernando Borretti <fernando@borretti.me>
2026-02-15 17:13:36 +1100

    remove todoist overlay

--- modules/todoist.nix
+++ modules/todoist.nix
@@ -9,20 +9,4 @@
   home-manager.users.eudoxia.home.packages = with pkgs; [
     todoist-electron
   ];
-
-  nixpkgs.overlays = [
-    (self: super: {
-      # This fixes a bug where `todoist-electron` thinks the timezone is
-      # `undefined` for some reason. Instead we explicitly set `TZ`.
-      todoist-electron = super.symlinkJoin {
-        name = "todoist-electron";
-        paths = [ super.todoist-electron ];
-        buildInputs = [ super.makeWrapper ];
-        postBuild = ''
-          wrapProgram $out/bin/todoist-electron \
-            --set TZ "Australia/Sydney"
-        '';
-      };
-    })
-  ];
 }
