757780995a1ea4273e1f5d145d819635afee4544
Fernando Borretti <fernando@borretti.me>
2026-02-15 18:25:23 +1100

    customize waybar style

--- modules/waybar/default.nix
+++ modules/waybar/default.nix
@@ -11,4 +11,5 @@
   ];
 
   home-manager.users.eudoxia.home.file.".config/waybar/config.jsonc".source = ./waybar.jsonc;
+  home-manager.users.eudoxia.home.file.".config/waybar/style.css".source = ./style.css;
 }
