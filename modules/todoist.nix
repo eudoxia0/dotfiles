784675ae3012f1c540dd0e159b49e6496b4c02ac
Fernando Borretti <fernando@borretti.me>
2026-02-15 17:35:25 +1100

    add todoist-wayland script

--- modules/todoist.nix
+++ modules/todoist.nix
@@ -5,8 +5,14 @@
   ...
 }:
 
+let
+  todoist-wayland = pkgs.writeShellScriptBin "todoist-wayland" ''
+    TZ=Australia/Sydney exec ${pkgs.todoist-electron}/bin/todoist-electron --ozone-platform-hint=auto "$@"
+  '';
+in
 {
   home-manager.users.eudoxia.home.packages = with pkgs; [
     todoist-electron
+    todoist-wayland
   ];
 }
