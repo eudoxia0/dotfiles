f5751c2e2eaf1276ffb122f08e16e2e2c7ce75d2
Fernando Borretti <fernando@borretti.me>
2026-02-16 16:52:10 +1100

    add custom fuzzel launcher

--- modules/fuzzel.nix
+++ modules/fuzzel.nix
@@ -5,8 +5,19 @@
   ...
 }:
 
+let
+  fuzzel-launcher = pkgs.writeShellScriptBin "fuzzel-launcher" ''
+    choice=$(printf "1Password\nZed\nCalibre" | fuzzel --dmenu)
+    case "$choice" in
+      "1Password") 1password ;;
+      "Zed") zed ;;
+      "Calibre") calibre ;;
+    esac
+  '';
+in
 {
   home-manager.users.eudoxia.home.packages = with pkgs; [
     fuzzel
+    fuzzel-launcher
   ];
 }
