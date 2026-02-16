7e52a5706b7c5a196e0a07a25516b1ed83edccf2
Fernando Borretti <fernando@borretti.me>
2026-02-16 11:21:49 +1100

    install kdotool and add myself to the input group

--- modules/espanso/default.nix
+++ modules/espanso/default.nix
@@ -6,7 +6,15 @@
 }:
 
 {
+  # Espanso on Wayland requires the input group for EVDEV access
+  users.users.eudoxia.extraGroups = [ "input" ];
+
   home-manager.users.eudoxia.services.espanso.enable = true;
+
+  # kdotool is required for text injection on Wayland
+  home-manager.users.eudoxia.home.packages = with pkgs; [
+    kdotool
+  ];
 
   home-manager.users.eudoxia.home.file = {
     ".config/espanso/match/base.yml".source = ./espanso.yaml;
