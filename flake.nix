d2ec12b901ec42378d6f53f281130841ba1b6d0b
Fernando Borretti <fernando@borretti.me>
2026-02-16 18:06:51 +1100

    disable espanso

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
         ./modules/docker.nix
         ./modules/emacs
         ./modules/epoch.nix
-        ./modules/espanso
+        # ./modules/espanso
         ./modules/eudoxia.nix
         ./modules/fastfetch
         ./modules/firefox
