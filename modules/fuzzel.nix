cf92ad5f698a853e5a8c3611500ecc643ffa85fc
Fernando Borretti <fernando@borretti.me>
2026-02-16 19:05:26 +1100

    add pavucontrol to fuzzel-launcher

--- modules/fuzzel.nix
+++ modules/fuzzel.nix
@@ -7,12 +7,13 @@
 
 let
   fuzzel-launcher = pkgs.writeShellScriptBin "fuzzel-launcher" ''
-    choice=$(printf "1Password\nCalibre\nDiet Tracker\nJournal\nTodoist\nZed" | fuzzel --dmenu)
+    choice=$(printf "1Password\nCalibre\nDiet Tracker\nJournal\nSound Settings\nTodoist\nZed" | fuzzel --dmenu)
     case "$choice" in
       "1Password") 1password ;;
       "Calibre") calibre ;;
       "Diet Tracker") chromium-browser --app=http://localhost:12001/ ;;
       "Journal") chromium-browser --app=http://localhost:12003/ ;;
+      "Sound Settings") pavucontrol ;;
       "Todoist") todoist-wayland ;;
       "Zed") zeditor ;;
     esac
