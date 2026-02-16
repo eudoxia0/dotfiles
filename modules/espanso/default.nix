506fdf75768322230814dac322e782691bfcda8a
Fernando Borretti <fernando@borretti.me>
2026-02-16 16:48:43 +1100

    fix espanso config

--- modules/espanso/default.nix
+++ modules/espanso/default.nix
@@ -9,6 +9,12 @@
   # Espanso on Wayland requires the input group for EVDEV access
   users.users.eudoxia.extraGroups = [ "input" ];
 
+  # Load uinput kernel module and set permissions for espanso
+  boot.kernelModules = [ "uinput" ];
+  services.udev.extraRules = ''
+    KERNEL=="uinput", GROUP="input", MODE="0660"
+  '';
+
   home-manager.users.eudoxia.services.espanso.enable = true;
 
   # kdotool is required for text injection on Wayland
