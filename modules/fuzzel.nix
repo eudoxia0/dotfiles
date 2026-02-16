b59f236ddacc52f470e29f1a3d6b1cb8c2c4bc99
Fernando Borretti <fernando@borretti.me>
2026-02-16 17:01:10 +1100

    add diet tracker to fuzzel-launcher

--- modules/fuzzel.nix
+++ modules/fuzzel.nix
@@ -7,10 +7,11 @@
 
 let
   fuzzel-launcher = pkgs.writeShellScriptBin "fuzzel-launcher" ''
-    choice=$(printf "1Password\nCalibre\nJournal\nTodoist\nZed" | fuzzel --dmenu)
+    choice=$(printf "1Password\nCalibre\nDiet Tracker\nJournal\nTodoist\nZed" | fuzzel --dmenu)
     case "$choice" in
       "1Password") 1password ;;
       "Calibre") calibre ;;
+      "Diet Tracker") chromium-browser --app=http://localhost:12001/ ;;
       "Journal") chromium-browser --app=http://localhost:12003/ ;;
       "Todoist") todoist-wayland ;;
       "Zed") zeditor ;;
