641261a462ffcd9d828a705a47f7aa3b691a41e0
Fernando Borretti <fernando@borretti.me>
2026-02-18 08:28:29 +1100

    revert

--- modules/espanso/default.nix
+++ modules/espanso/default.nix
@@ -15,7 +15,9 @@
     KERNEL=="uinput", GROUP="input", MODE="0660"
   '';
 
-  home-manager.users.eudoxia.services.espanso.enable = true;
+  home-manager.users.eudoxia.services.espanso = {
+    enable = true;
+  };
 
   # kdotool is required for text injection on Wayland
   # wl-clipboard provides wl-copy/wl-paste for clipboard-based injection
