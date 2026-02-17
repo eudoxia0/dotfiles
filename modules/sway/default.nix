c9ce06e548ca3755598c9170db481abb2e6e248b
Fernando Borretti <fernando@borretti.me>
2026-02-18 07:55:22 +1100

    working emoji picker with ydotool

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -9,6 +9,14 @@ let
   take-screenshot = pkgs.writeShellScriptBin "take-screenshot" ''
     grim -g "$(slurp)" ~/screenshot.png
   '';
+
+  # Emoji picker using bemoji with ydotool backend (works with Chromium)
+  emoji-picker = pkgs.writeShellScriptBin "emoji-picker" ''
+    export BEMOJI_PICKER_CMD="${pkgs.wofi}/bin/wofi -d -i -p emoji"
+    export BEMOJI_TYPE_CMD="ydotool type --"
+    export BEMOJI_CLIP_CMD="${pkgs.wl-clipboard}/bin/wl-copy"
+    exec ${pkgs.bemoji}/bin/bemoji -t
+  '';
 in
 {
   # Sway window manager configuration
@@ -22,7 +30,8 @@ in
       wl-clipboard
       wlsunset
       swaylock
-      wofi-emoji
+      bemoji
+      emoji-picker
       take-screenshot
     ];
   };
