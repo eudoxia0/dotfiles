3beee11dc8570211b7c0729f413762b2dd2bc03e
Fernando Borretti <fernando@borretti.me>
2026-02-17 18:27:41 +1100

    install nm-applet through home-manager

--- modules/network.nix
+++ modules/network.nix
@@ -8,6 +8,14 @@
 {
   networking.networkmanager.enable = true;
   networking.firewall.enable = true;
-  programs.nm-applet.enable = true;
   services.mullvad-vpn.enable = true;
+
+  home-manager.users.eudoxia.services = {
+    network-manager-applet.enable = true;
+  };
+
+  # If nm-applet is not instaled explicitly it won't have icons.
+  home-manager.users.eudoxia.home.packages = with pkgs; [
+    pkgs.networkmanagerapplet
+  ];
 }
