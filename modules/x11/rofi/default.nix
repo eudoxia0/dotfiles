1f428ab1901278b9d530d06916d22673e6f2ee38
Fernando Borretti <fernando@borretti.me>
2026-02-27 12:30:57 +1100

    rofi-launcher: add obsidian

--- modules/x11/rofi/default.nix
+++ modules/x11/rofi/default.nix
@@ -7,7 +7,7 @@
 
 let
   rofi-launcher = pkgs.writeShellScriptBin "rofi-launcher" ''
-    choice=$(printf "1Password\nCalibre\nChromium\nDiet Tracker\nJournal\nLock Screen\nScreenshot\nSignal\nSound Settings\nTodoist\nZed" | rofi -i -dmenu)
+    choice=$(printf "1Password\nCalibre\nChromium\nDiet Tracker\nJournal\nLock Screen\nObsidian\nScreenshot\nSignal\nSound Settings\nTodoist\nZed" | rofi -i -dmenu)
     case "$choice" in
       "1Password") 1password ;;
       "Calibre") calibre ;;
@@ -15,6 +15,7 @@ let
       "Diet Tracker") chromium-browser --app=http://localhost:12001/ ;;
       "Journal") chromium-browser --app=http://localhost:12003/ ;;
       "Lock Screen") xscreensaver-command -lock ;;
+      "Obsidian") obsidian ;;
       "Screenshot") scrot -f -s ;;
       "Signal") signal-desktop ;;
       "Sound Settings") pavucontrol ;;
