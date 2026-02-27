8e5f89b665b9faf908bc7429d49cfeb1560d5615
Fernando Borretti <fernando@borretti.me>
2026-02-27 12:31:20 +1100

    rofi-launcher: rename 'Lock Screen' to 'Lock' to prevent collision

--- modules/x11/rofi/default.nix
+++ modules/x11/rofi/default.nix
@@ -7,14 +7,14 @@
 
 let
   rofi-launcher = pkgs.writeShellScriptBin "rofi-launcher" ''
-    choice=$(printf "1Password\nCalibre\nChromium\nDiet Tracker\nJournal\nLock Screen\nObsidian\nScreenshot\nSignal\nSound Settings\nTodoist\nZed" | rofi -i -dmenu)
+    choice=$(printf "1Password\nCalibre\nChromium\nDiet Tracker\nJournal\nLock\nObsidian\nScreenshot\nSignal\nSound Settings\nTodoist\nZed" | rofi -i -dmenu)
     case "$choice" in
       "1Password") 1password ;;
       "Calibre") calibre ;;
       "Chromium") chromium-browser ;;
       "Diet Tracker") chromium-browser --app=http://localhost:12001/ ;;
       "Journal") chromium-browser --app=http://localhost:12003/ ;;
-      "Lock Screen") xscreensaver-command -lock ;;
+      "Lock") xscreensaver-command -lock ;;
       "Obsidian") obsidian ;;
       "Screenshot") scrot -f -s ;;
       "Signal") signal-desktop ;;
