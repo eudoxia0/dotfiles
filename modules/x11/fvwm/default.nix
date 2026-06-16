810d2d0615c116f9998fc93ab11298e8237f88c4
Fernando Borretti <fernando@borretti.me>
2026-06-16 18:55:15 +1000

    fvwm: cde colors menu

--- modules/x11/fvwm/default.nix
+++ modules/x11/fvwm/default.nix
@@ -10,9 +10,16 @@
     enable = true;
   };
 
+  # Copy the CDE colors directory.
+  home-manager.users.eudoxia.home.file.".fvwm/CdeColors" = {
+    source = ./cde;
+    recursive = true;
+  };
+
   home-manager.users.eudoxia.home.file = {
     ".fvwm/config".source = ./fvwm.txt;
   };
+
   home-manager.users.eudoxia.home.packages = with pkgs; [
     stalonetray
   ];
