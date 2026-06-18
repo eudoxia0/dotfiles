ff6e22b7037ebc41283c566ec1963d9f89bd958b
Fernando Borretti <fernando@borretti.me>
2026-06-18 10:48:03 +1000

    fmt

--- modules/x11/fvwm/default.nix
+++ modules/x11/fvwm/default.nix
@@ -15,7 +15,8 @@
       packages = [ pkgs.stalonetray ];
       file = {
         # Symlink the fvwm config.
-        ".fvwm/config".source = hm.config.lib.file.mkOutOfStoreSymlink "${hm.config.home.homeDirectory}/root/1-workspace/dotfiles/modules/x11/fvwm/fvwm.txt";
+        ".fvwm/config".source =
+          hm.config.lib.file.mkOutOfStoreSymlink "${hm.config.home.homeDirectory}/root/1-workspace/dotfiles/modules/x11/fvwm/fvwm.txt";
         # Copy the CDE colors directory.
         ".fvwm/CdeColors" = {
           source = ./cde;
