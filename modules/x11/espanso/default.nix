5e310e3833213c93887c6b9c2a2f817c8417c466
Fernando Borretti <fernando@borretti.me>
2026-06-18 10:38:11 +1000

    use a symlink

--- modules/x11/espanso/default.nix
+++ modules/x11/espanso/default.nix
@@ -12,11 +12,10 @@
     KERNEL=="uinput", GROUP="input", MODE="0660"
   '';
 
-  home-manager.users.eudoxia.services.espanso = {
-    enable = true;
-  };
+  home-manager.users.eudoxia = hm: {
+    services.espanso.enable = true;
 
-  home-manager.users.eudoxia.home.file = {
-    ".config/espanso/match/base.yml".source = ./espanso.yaml;
+    home.file.".config/espanso/match/base.yml".source =
+      hm.config.lib.file.mkOutOfStoreSymlink "${hm.config.home.homeDirectory}/root/1-workspace/dotfiles/espanso.yaml";
   };
 }
