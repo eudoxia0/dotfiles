3508c0830ed04fe16df532b54b1d1566cb2b07d2
Fernando Borretti <fernando@borretti.me>
2026-07-04 20:38:51 +0800

    punct.py: allow reading from a file

--- modules/scripts/punct.py
+++ modules/scripts/punct.py
@@ -11,3 +11,22 @@ for c in sys.stdin.read():
     if c in PUNCT:
         print(c, end="")
 print()
+
+
+def main(text: str):
+    for c in text:
+        if c in PUNCT:
+            print(c, end="")
+    print()
+
+
+if __name__ == "__main__":
+    argc: int = len(sys.argv)
+    if argc == 2:
+        with open(sys.argv[1]) as stream:
+            main(stream.read())
+    elif argc == 1:
+        main(sys.stdin.read())
+    else:
+        print("Wrong number of arguments.", file=sys.stderr)
+        exit(-1)
