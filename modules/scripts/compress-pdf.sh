da197b59f8f0f3fb6d2d6f9a0b8206a0f9e97e18
Fernando Borretti <fernando@borretti.me>
2026-07-13 05:56:39 +0800

    compress-pdf: don't use quiet output

--- modules/scripts/compress-pdf.sh
+++ modules/scripts/compress-pdf.sh
@@ -4,4 +4,4 @@
 #
 #     compress-pdf input.pdf output.pdf
 
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=$2 $1
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dBATCH -sOutputFile=$2 $1
