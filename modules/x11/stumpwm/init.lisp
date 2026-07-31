d9109b551246357f0e85a15189dd43331030af28
Fernando Borretti <fernando@borretti.me>
2026-07-31 12:44:17 +1000

    stumpwm: undefine keybindings

--- modules/x11/stumpwm/init.lisp
+++ modules/x11/stumpwm/init.lisp
@@ -96,7 +96,7 @@
 
 ;; Remove default keybindings I don't use.
 (defparameter *keybinds-to-remove*
-  (list "c" "C-c" "e" "C-e" "C-a" "!" "w" "x" "F1" "F2" "F3" "F4" "F5" "F6" "F7" "F8" "F9" "F10" "C-0"))
+  (list "c" "C-c" "e" "C-e" "C-a" "!" "w" "x" "F1" "F2" "F3" "F4" "F5" "F6" "F7" "F8" "F9" "F10" "C-0" "C-1" "C-2" "C-3" "C-4" "C-5" "C-6" "C-7" "C-8" "C-9"))
 
 (dolist (key *keybinds-to-remove*)
   (undefine-key *root-map* (kbd key)))
