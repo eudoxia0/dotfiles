8584bc410cfa90776dea113d6ea046fc24c907ef
Fernando Borretti <fernando@borretti.me>
2026-08-10 07:42:17 +1000

    stumpwm: xsecurelock command

--- modules/x11/stumpwm/init.lisp
+++ modules/x11/stumpwm/init.lisp
@@ -197,6 +197,9 @@
 (defkey "Pause" "exec xscreensaver-command --lock")
 (define-key *root-map* (kbd "x") "exec xscreensaver-command --lock")
 
+;; Lock the screen with xsecurelock (no screensaver).
+(define-key *root-map* (kbd "X") "exec xsecurelock")
+
 ;; Toggle the modeline on/off.
 (define-key *root-map* (kbd "m") "mode-line")
 
