4905ea6d3afca0b2b30df981b8ea7e285369f354
Fernando Borretti <fernando@borretti.me>
2026-08-25 18:05:26 +1000

    rename

--- hosts/miranda/configuration.nix
+++ hosts/miranda/configuration.nix
@@ -21,8 +21,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
