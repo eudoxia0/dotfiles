f7a70872e99c15d2e7e821e5d13cc4cdc400afa8
Fernando Borretti <fernando@borretti.me>
2026-08-25 17:58:33 +1000

    trim

--- hosts/miranda/configuration.nix
+++ hosts/miranda/configuration.nix
@@ -41,9 +41,6 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   # Install firefox.
   programs.firefox.enable = true;
 
