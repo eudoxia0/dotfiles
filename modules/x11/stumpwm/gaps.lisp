802843e27c301c8308ce3ac5a50c81f14d422abf
Fernando Borretti <fernando@borretti.me>
2026-08-27 10:03:21 +1000

    stumpwm: change gap size

--- modules/x11/stumpwm/gaps.lisp
+++ modules/x11/stumpwm/gaps.lisp
@@ -11,11 +11,11 @@
 (defparameter *head-gaps-size* 0)
 
 ;; Inner gaps run along all the 4 borders of a window.
-(defparameter *inner-gaps-size* 16)
+(defparameter *inner-gaps-size* 32)
 
 ;; Outer gaps add more padding to the outermost borders of a window
 ;; (touching the screen border).
-(defparameter *outer-gaps-size* 16)
+(defparameter *outer-gaps-size* 32)
 
 ;; Whether or not gaps are on.
 (defparameter *gaps-on* nil)
