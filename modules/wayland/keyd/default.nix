44cac9c48295b71cce31da238de988ef7af35c32
Fernando Borretti <fernando@borretti.me>
2026-09-20 16:21:20 +1000

    keyd: configure xcape replacement

--- modules/wayland/keyd/default.nix
+++ modules/wayland/keyd/default.nix
@@ -11,4 +11,18 @@
   users.users.eudoxia.packages = with pkgs; [
     keyd
   ];
+
+  services.keyd.keyboards = {
+    default = {
+      ids = [
+        "*"
+      ];
+      settings = {
+        main = {
+          "leftshift" = "overload(shift, S-9)";
+          "rightshift" = "overload(shift, S-0)";
+        };
+      };
+    };
+  };
 }
