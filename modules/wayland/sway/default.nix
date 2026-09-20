cc59a4c02e7c357f940a01bf077cb7870b90c250
Fernando Borretti <fernando@borretti.me>
2026-09-20 13:33:27 +1000

    install swaybg

--- modules/wayland/sway/default.nix
+++ modules/wayland/sway/default.nix
@@ -12,6 +12,10 @@
     wrapperFeatures.gtk = true;
   };
 
+  users.users.eudoxia.packages = with pkgs; [
+    swaybg
+  ];
+
   systemd.tmpfiles.rules = [
     "L+ /home/eudoxia/.config/sway/config - - - - ${dotfilesDir}/modules/wayland/sway/sway.conf"
     "L+ /home/eudoxia/.config/swaylock/config - - - - ${dotfilesDir}/modules/wayland/sway/swaylock.conf"
