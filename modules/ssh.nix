c7e789174a743bf64d43c8c463bdab8c565432c4
Fernando Borretti <fernando@borretti.me>
2026-09-22 09:24:37 +1000

    fix ssh config

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -13,4 +13,8 @@
 
   services.gnome.gnome-keyring.enable = true;
   services.gnome.gcr-ssh-agent.enable = true;
+
+  environment.sessionVariables = {
+    SSH_AUTH_SOCK = "$XDG_RUNTIME_DIR/gcr/ssh";
+  };
 }
