a693bb84c0d8dabb7e28020b67fd553bd91512e1
Fernando Borretti <fernando@borretti.me>
2026-09-24 19:01:34 +1000

    rostam: on wayland, set all scaling env vars to 1

--- hosts/rostam/wayland.nix
+++ hosts/rostam/wayland.nix
@@ -6,6 +6,16 @@
 }:
 
 {
+  # Monitor scaling.
+  environment.sessionVariables = {
+    # Scale GTK apps. Integer.
+    GDK_SCALE = "1";
+    # Scale GTK font sizes. Real number.
+    GDK_DPI_SCALE = "1";
+    # Scale GT apps. Real number.
+    QT_SCALE_FACTOR = "1";
+  };
+
   systemd.tmpfiles.rules = [
     "L+ /home/eudoxia/.config/sway/extra/rostam - - - - ${dotfilesDir}/hosts/rostam/sway.conf"
   ];
