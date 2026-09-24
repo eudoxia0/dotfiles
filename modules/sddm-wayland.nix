36f29a24abb6309aeea09aa29efeba4846494420
Fernando Borretti <fernando@borretti.me>
2026-09-24 19:22:43 +1000

    sddm: more wayland cursor fixes

--- modules/sddm-wayland.nix
+++ modules/sddm-wayland.nix
@@ -8,4 +8,7 @@
 {
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.wayland.compositor = "kwin";
+  systemd.services.display-manager.environment = {
+    KWIN_FORCE_SW_CURSOR = "1";
+  }
 }
